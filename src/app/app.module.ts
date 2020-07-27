b820a2c8c0e77493f9872cd45673dd1ff9dae16a
MariyaSin <maria1991@list.ru>
2020-07-27 17:03:06 +0300

    fixed app.module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { ChartsModule } from "ng2-charts";
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ChartComponent } from './chart/chart.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ChartComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    ChartsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
