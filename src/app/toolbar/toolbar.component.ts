521b9a2bf6d6841323c95e99b3c6795f70bb46e0
AleksandraPurrple <65856130+AleksandraPurrple@users.noreply.github.com>
2020-07-29 16:53:33 +0300

    Added red light of notification, user photo

--- src/app/toolbar/toolbar.component.ts
+++ src/app/toolbar/toolbar.component.ts
@@ -6,10 +6,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./toolbar.component.scss']
 })
 export class ToolbarComponent implements OnInit {
-
+  hasNotification = false;
   constructor() { }
 
   ngOnInit() {
   }
 
+  onClick(){
+    this.hasNotification = !this.hasNotification;
+  }
 }
