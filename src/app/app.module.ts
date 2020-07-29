7c91807e79a1c2913c650d005404fb20201d2444
AnnaRubanenko <aneyta@mail.ru>
2020-07-29 15:52:47 +0300

    Значения передаются через интерполяцию

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import {DoSelectComponent} from './do-select/do-select.component';
+// import {ProjectService, TitleService} from './options.service.';
 
 @NgModule({
   declarations: [
--- src/app/do-select/do-select.component.ts
+++ src/app/do-select/do-select.component.ts
@@ -1,9 +1,25 @@
-import { Component } from '@angular/core';
+import {Component, Input} from '@angular/core';
+
+
+
 
 @Component({
   selector: 'app-select',
   templateUrl: './do-select.component.html',
-  styleUrls: ['./do-select.component.scss']
+  styleUrls: ['./do-select.component.scss'],
+
+
 })
-export class DoSelectComponent {}
+export class DoSelectComponent {
+
+ @Input() pro;
+  optionsPro = ['first', 'second'];
+  @Input() options: string[];
+  @Input() className: string;
+  @Input() title: string;
+
+  constructor() {}
+
+
+}
 
