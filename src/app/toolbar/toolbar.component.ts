39b89648c62b61217c6e096206ff42459b672842
AleksandraPurrple <65856130+AleksandraPurrple@users.noreply.github.com>
2020-07-31 15:36:26 +0300

    Сhanged methods

--- src/app/toolbar/toolbar.component.ts
+++ src/app/toolbar/toolbar.component.ts
@@ -1,23 +1,16 @@
 import { Input, Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-toolbar',
+  selector: 'do-toolbar',
   templateUrl: './toolbar.component.html',
   styleUrls: ['./toolbar.component.scss']
 })
 export class ToolbarComponent implements OnInit {
   hasNotification = false;
-  isDarkMood = true;
+  isDarkMode = true;
   @Input() userName;
   constructor() { }
 
   ngOnInit() {
-  }
-
-  onClickNotification() {
-    this.hasNotification = !this.hasNotification;
-  }
-  onClickMood() {
-    this.isDarkMood = !this.isDarkMood;
   }
 }
