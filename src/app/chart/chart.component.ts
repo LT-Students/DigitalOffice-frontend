d30f1afe1e5b837c5e089bd431cf642ae97bf737
MariyaSin <maria1991@list.ru>
2020-07-31 03:07:06 +0300

    added empty chart

--- src/app/chart/chart.component.ts
+++ src/app/chart/chart.component.ts
@@ -1,30 +1,34 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 import { ElementRef} from "@angular/core";
 import { ViewChild} from "@angular/core";
 import {count} from "rxjs/operators";
 import { Chart } from 'chart.js';
 
 @Component({
-  selector: 'app-chart',
+  selector: 'do-chart',
   templateUrl: './chart.component.html',
   styleUrls: ['./chart.component.scss']
 })
 
 export class ChartComponent implements OnInit {
   constructor() { }
+  @Input() projectsData;               //Data about projectsName and projectsHours
+  @Input() allHoursPlan;               //Hours of plan
+  @Input() timeDescrValue;             //Difference between hours in the center under hours
 
   colorsData = ["#7C799B", "#C7C6D8", "#FFB2B2", "#FFB78C", "#EB5757", "#BC7BFA", "#FFBE97", "#BDBDBD"];
 
-  projectsData = [
-    {projectName: "Kojima", projectHours: "10.26"},
-    {projectName: "First", projectHours: "35.7"},
-    {projectName: "Something", projectHours: "15.3"},
-    {projectName: "Else", projectHours: "15"},
-    ];
+  //variables for hours in the center
+  allHoursPerfom;
+  allHoursPrint;
+  allHoursColor;
+  //
 
-  allHoursValue
-  hoursDiffValue = -0.03
-  backgroundColor
+  //variables for string under hours in the center of the chart
+  timeDescrBackgroundColor;
+  timeDescrFontSize;
+  timeDescrColor;
+  //
 
   @ViewChild('canvas', { static: true })
   canvas: ElementRef<HTMLCanvasElement>;
@@ -32,38 +36,76 @@ export class ChartComponent implements OnInit {
 
   ngOnInit() {
     //calculating the total number of hours in the center of chart
-    this.allHoursValue = 0;
-    for (let i = 0; i < this.projectsData.length; i++) {
-      this.allHoursValue += Number(this.projectsData[i].projectHours);
-    }
-    //
 
-    //defining the background-color for the square in the center of the chart
-    if (this.allHoursValue > 0) {
-      this.backgroundColor = "#21D373";
-    } else {
-      this.backgroundColor = "#EB5757";
+    this.allHoursPerfom = new Date();
+    this.allHoursPerfom = 0;
+    for (let i = 0; i < this.projectsData.length; i++) {
+      this.allHoursPerfom += Number (this.projectsData[i].projectHours);
     }
     //
 
     //donut building
-      this.ctx = this.canvas.nativeElement.getContext('2d');
-    var myChart = new Chart(this.ctx, {
-      type: 'doughnut',
+    this.ctx = this.canvas.nativeElement.getContext('2d');
 
-      data: {
-        datasets: [{
-          data: this.projectsData.map((project) => project.projectHours),
-          backgroundColor: this.colorsData,
-          borderWidth: 0,
-        }]
-      },
-      options: {
-        cutoutPercentage: 70,
-        tooltips: {
-          enabled: false
+    if (this.allHoursPerfom > 0) {
+
+      //Hours in the center
+      this.allHoursPrint = this.allHoursPerfom;
+      this.allHoursColor = "#434348";
+      //
+
+      //String under hours
+      this.timeDescrColor = "#FFFFFF";
+      this.timeDescrValue >= 0 ? this.timeDescrBackgroundColor = "#21D373" : this.timeDescrBackgroundColor = "#EB5757";
+      this.timeDescrFontSize = "16px";
+      //
+
+      var myChart = new Chart(this.ctx, {
+        type: 'doughnut',
+        data: {
+          datasets: [{
+            data: this.projectsData.map((project) => project.projectHours),
+            backgroundColor: this.colorsData,
+            borderWidth: 0,
+          }]
+        },
+        options: {
+          cutoutPercentage: 70,
+          tooltips: {
+            enabled: false
+          }
         }
-      }
-    });
+      });
+    } else {
+
+      //Hours in the center
+      this.allHoursPrint = this.allHoursPlan;
+      this.allHoursColor = "#BDBDBD";
+      //
+
+      //String under hours
+      this.timeDescrValue = "Запаланированно";
+      this.timeDescrBackgroundColor = "#FFFFFF";
+      this.timeDescrColor = "#BDBDBD";
+      this.timeDescrFontSize = "14px";
+      //
+
+      var myChart = new Chart(this.ctx, {
+        type: 'doughnut',
+        data: {
+          datasets: [{
+            data: [1],
+            backgroundColor: "#F1F1EF",
+            borderWidth: 0,
+          }]
+        },
+        options: {
+          cutoutPercentage: 70,
+          tooltips: {
+            enabled: false
+          }
+        }
+      });
     }
+  }
 }
