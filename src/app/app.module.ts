bd4ce4fe91946553e1748c65c6628da33b7a2cfe
NGurev <Nikita.Gurev@lanit-tercom.com>
2020-08-03 17:50:56 +0300

    правки по ревью

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import {DoSelectComponent} from './do-select/do-select.component';
+import { RouterModule } from '@angular/router';
 // import {ProjectService, TitleService} from './options.service.';
 
 @NgModule({
@@ -11,7 +12,8 @@ import {DoSelectComponent} from './do-select/do-select.component';
     DoSelectComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/do-select/do-select.component.ts
+++ src/app/do-select/do-select.component.ts
@@ -4,7 +4,7 @@ import {Component, Input} from '@angular/core';
 
 
 @Component({
-  selector: 'app-select',
+  selector: 'do-select',
   templateUrl: './do-select.component.html',
   styleUrls: ['./do-select.component.scss'],
 
@@ -12,8 +12,6 @@ import {Component, Input} from '@angular/core';
 })
 export class DoSelectComponent {
 
- @Input() pro;
-  optionsPro = ['first', 'second'];
   @Input() options: string[];
   @Input() className: string;
   @Input() title: string;
