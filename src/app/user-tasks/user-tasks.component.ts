7c6ff79669dc15c0a77f725cfd7a0aaa1f649908
Artem Atroshchenko <actpo4kun@gmail.com>
2020-08-10 21:11:20 +0300

    Fixed minor lines.

--- src/app/user-tasks/user-tasks.component.ts
+++ src/app/user-tasks/user-tasks.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input, OnInit, } from '@angular/core';
-/*import {IProject} from '../models/project.model';*/
+import { IProject } from '../models/project.model';
+import { ITask } from '../models/task.model';
 
 @Component({
   selector: 'do-user-tasks',
@@ -7,9 +8,31 @@ import { Component, Input, OnInit, } from '@angular/core';
   styleUrls: ['./user-tasks.component.scss']
 })
 export class UserTasksComponent implements OnInit {
-  @Input() projects: Array<IProject>;
+  @Input() projects: Array<IProject> = [];
+  
   constructor() {}
-  ngOnInit() {}
+  
+  ngOnInit() {
+    var project = new Project();
+    project.name = 'Some project';
+    project.tasks = [
+        {
+          time: { hours: 4, minutes: 0},
+          name: 'Some task',
+          description: 'Some description',
+          createdAt: new Date(2017, 1)
+        }
+      ];
+
+    this.projects.push(
+      project
+    )
+  }
 
 }
-export interface IProject{}
+export class Project implements IProject{
+  name: string;
+  tasks: Array<ITask>;
+}
+
+
