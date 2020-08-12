613cbea7a6388ea4b876cd47a4d2f066e378eb92
Artem Atroshchenko <actpo4kun@gmail.com>
2020-08-12 23:38:57 +0300

    Created template and styles for new-employee component.

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -5,6 +5,7 @@ import { AdminComponent } from './admin/admin.component';
 import { RouterModule } from '@angular/router';
 import { AdminRoutingModule } from './admin-routing.module';
 import { NewEmployeeComponent } from './new-employee/new-employee.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 
 
@@ -17,6 +18,8 @@ import { NewEmployeeComponent } from './new-employee/new-employee.component';
   imports: [
     CommonModule,
     RouterModule,
+    FormsModule,
+    ReactiveFormsModule,
     AdminRoutingModule
   ]
 })
--- src/app/admin/new-employee/new-employee.component.ts
+++ src/app/admin/new-employee/new-employee.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'do-new-employee',
@@ -6,10 +7,45 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./new-employee.component.scss']
 })
 export class NewEmployeeComponent implements OnInit {
-
-  constructor() { }
+  
+  private userForm: FormGroup;
+  private positions = [
+    { name : 'position1'},
+    { name : 'position2'},
+    { name : 'position3'},
+  ];
+  private rates = [
+    0,
+    0.5,
+    1,
+    1.5,
+    2
+  ]
+  private departments = [
+    { name: 'department1' },
+    { name: 'department2' },
+    { name: 'department3' }
+  ]
+  
+  constructor(
+    private formBuilder: FormBuilder
+  ) { 
+    this.userForm = this.formBuilder.group({
+      lastName: ['', Validators.required],
+      firstName: ['', Validators.required],
+      middleName: ['', Validators.required],
+      position: ['', Validators.required],
+      rate: ['', Validators.required],
+      department: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', Validators.required],
+    });
+  }
 
   ngOnInit(): void {
   }
 
+  generateCredentials(): void {
+    
+  }
 }
