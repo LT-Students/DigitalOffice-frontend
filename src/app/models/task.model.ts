5e40fcf314a6d8ac6877af3e432f032f8eaf6b7e
AleksandraPurrple <65856130+AleksandraPurrple@users.noreply.github.com>
2020-08-16 00:37:57 +0300

    Cleaned code for pull request.
    Cleaned code for pull request.
    Added output for time in task component.

--- src/app/models/task.model.ts
+++ src/app/models/task.model.ts
@@ -1,8 +1,7 @@
 import {Time} from "@angular/common";
 
 export interface ITask {
-  /*time: Time;*/
-  time: Date;
+  time: Time;
   name: string;
   description: string;
   createdAt: Date;
--- src/app/user-tasks/user-tasks.component.ts
+++ src/app/user-tasks/user-tasks.component.ts
@@ -1,6 +1,5 @@
 import { Component, Input, OnInit, } from '@angular/core';
 import { IProject } from '../models/project.model';
-import { ITask } from '../models/task.model';
 
 
 @Component({
@@ -14,27 +13,8 @@ export class UserTasksComponent implements OnInit {
   constructor() {}
 
   ngOnInit() {
-    var project = new Project();
-    project.name = 'Kojima is a genius';
-    project.tasks = [
-        {
-          time: new Date(),
-          /*time: { hours: 4 , minutes: 0},*/
-          name: 'Фикс неправильного отображения строки “Коты” на дашборде',
-          description: 'Распланировали сегодня спринт на 11 дней, дал задачи стажеру. Я просто решила написать длинный комментарий, чтобы посмотреть, как сломается система, потому что еще работаю на полставки тестировщиком. Му-ха-ха-ха!',
-          createdAt: new Date()
-          /*createdAt: new Date(2020, 6)*/
-        }
-      ];
 
-    this.projects.push(
-      project
-    )
   }
 
-}
-export class Project implements IProject{
-  name: string;
-  tasks: Array<ITask>;
 }
 
