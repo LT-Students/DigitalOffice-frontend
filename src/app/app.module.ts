b9bf5b4af7202340aadf7f9fd4c532d7a5ecabef
AnastassiaVarabei <61653983+AnastassiaVarabei@users.noreply.github.com>
2020-08-20 18:00:31 +0300

    Изменена функция по динамическим датам и добавлены данные в app.modul… (#21)
    * Изменена функция по динамическим датам и добавлены данные в app.module.ts для написания на русском языке
    
    * переименование переменных
    
    * added text-align: center for week day

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,7 +9,14 @@ import { ToolbarComponent } from './toolbar/toolbar.component';
 import { DateDescComponent } from './date-desc/date-desc.component';
 import { ButtonComponent } from './button/button.component';
 import { TagsBlockComponent } from './tags-block/tags-block.component';
+import { LOCALE_ID} from '@angular/core';
+import { registerLocaleData} from '@angular/common';
+import localeRu from '@angular/common/locales/ru';
+
+registerLocaleData(localeRu);
+
 import { AppRoutingModule } from './app-routing.module';
+
 
 @NgModule({
   declarations: [
@@ -26,7 +33,9 @@ import { AppRoutingModule } from './app-routing.module';
     AuthModule,
     RouterModule
   ],
-  providers: [],
+  providers: [
+    {provide: LOCALE_ID, useValue: 'ru-RU'}
+    ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
