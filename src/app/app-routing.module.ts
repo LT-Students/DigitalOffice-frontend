1e400dad085da287181c7ae98d28fe9ac6f38de2
MariyaSin <maria1991@list.ru>
2020-08-23 13:21:42 +0300

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,26 @@ import { Routes, RouterModule } from '@angular/router';
 import { AuthGuardService } from './services/auth-guard.service';
 import { DateDescComponent } from './date-desc/date-desc.component';
 import { AttendanceComponent } from './attendance/attendance.component';
+import {ChartComponent} from "./chart/chart.component";
+import {LoginComponent} from "./auth/login/login.component";
 
 
 const routes: Routes = [
+
   { path: '', redirectTo: '/auth/login', pathMatch: 'full' },
-  { path: 'attendance', component: AttendanceComponent }
+  { path: 'attendance', component: AttendanceComponent, children: [
+      { path: '', component: AttendanceComponent },
+      { path: 'trrak', redirectTo: '/attendance', pathMatch: 'full'}
+    ] },
+  /*{ path: 'admin', component: AdminComponent, children: [
+      { path: '', component: DashboardComponent },
+      { path: 'dashboard', component: DashboardComponent },
+      { path: 'trrak', redirectTo: '/attendance', pathMatch: 'full' }
+      { path: 'addEmployee', component: AddEmployee },
+      { path: 'addCompany', component: AddCompany },
+    ]},*/
+
+
 ];
 
 @NgModule({
