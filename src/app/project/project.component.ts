914ec96aa23c3859592d607b0612b482bdd821a3
AnthonyBassplayer <67804740+AnthonyBassplayer@users.noreply.github.com>
2020-08-25 02:32:56 +0300

    Do user notes (#18)
    * do-user-notes
    
    * task added
    
    * interfaces edits
    
    * models edited
    
    * edits
    
    * Edited
    
    * Fixed minor lines.
    
    * New edits
    
    * major style fixes
    
    * project and task edited
    
    * Added buttons and search line
    
    * date pipes and minor styles
    
    * task time edits
    
    * Cleaned code for pull request.
    
    Cleaned code for pull request.
    Added output for time in task component.
    
    * Fixed quotemark warnings.
    
    * Changed buttons
    
    * Intermediate changes in user task component
    
    * Missed changes
    
    * Nikitos edits done
    
    * Added date changes, fixed code
    
    * Delete user-tasks.component.css
    
    * Delete user-tasks.component.css.map
    
    * добавлены тестовые данных
    
    * DO-12: правки по верстке, переименование файлов
    
    Co-authored-by: AleksandraPurrple <65856130+AleksandraPurrple@users.noreply.github.com>
    Co-authored-by: Artem Atroshchenko <actpo4kun@gmail.com>
    Co-authored-by: NGurev <Nikita.Gurev@lanit-tercom.com>

---
+++ src/app/project/project.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit, Input } from '@angular/core';
+import {IProject} from '../interfaces/project.interface';
+
+
+@Component({
+  selector: 'do-project',
+  templateUrl: './project.component.html',
+  styleUrls: ['./project.component.scss']
+})
+export class ProjectComponent implements OnInit {
+
+  @Input() project: IProject;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
