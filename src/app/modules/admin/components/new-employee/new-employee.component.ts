245e1eaf8138b7c34de5e97ed43911b72e01ceaa
NGurev <Nikita.Gurev@lanit-tercom.com>
2020-08-27 01:25:48 +0300

    DO-23: Добавление и превью фотографии

--- src/app/modules/admin/components/new-employee/new-employee.component.ts
+++ src/app/modules/admin/components/new-employee/new-employee.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
@@ -7,7 +7,11 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
   styleUrls: ['./new-employee.component.scss']
 })
 export class NewEmployeeComponent implements OnInit {
-  
+
+  public message: string;
+  public imagePath;
+  imgURL: any;
+
   private userForm: FormGroup;
   private positions = [
     { name : 'position1'},
@@ -26,10 +30,10 @@ export class NewEmployeeComponent implements OnInit {
     { name: 'department2' },
     { name: 'department3' }
   ]
-  
+
   constructor(
     private formBuilder: FormBuilder
-  ) { 
+  ) {
     this.userForm = this.formBuilder.group({
       lastName: ['', Validators.required],
       firstName: ['', Validators.required],
@@ -50,10 +54,28 @@ export class NewEmployeeComponent implements OnInit {
   }
 
   sendCredentials(): void {
-    
+
   }
 
   generateCredentials(): void {
-    
+
+  }
+
+  preview(files) {
+    if (files.length === 0)
+      return;
+
+    var mimeType = files[0].type;
+    if (mimeType.match(/image\/*/) == null) {
+      this.message = "Only images are supported.";
+      return;
+    }
+
+    var reader = new FileReader();
+    this.imagePath = files;
+    reader.readAsDataURL(files[0]);
+    reader.onload = (_event) => {
+      this.imgURL = reader.result;
+    }
   }
 }
