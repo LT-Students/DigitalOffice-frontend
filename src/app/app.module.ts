07a18fa60d7aedcc79372adc2b09913bdcea26b6
Anastassia Varabei <nastenkaok1984@gmail.com>
2020-09-08 08:47:50 +0300

    added component new-project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -19,12 +19,14 @@ import localeRu from '@angular/common/locales/ru';
 import { SharedModule } from './modules/shared/shared.module';
 import { UserPageModule } from './modules/user-page/user-page.module';
 import { AttendanceService } from './modules/user-page/components/attendance/attendance.service';
+import { NewProjectComponent } from './modules/admin/components/new-project/new-project.component';
 
 registerLocaleData(localeRu);
 
 @NgModule({
   declarations: [
     AppComponent,
+    NewProjectComponent,
   ],
   imports: [
     BrowserModule,
