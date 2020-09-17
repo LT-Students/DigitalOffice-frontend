0a740036f550e2892b1f4ceef95f1037910f3606
Mariya Murasheva <xomnsn@gmail.com>
2020-09-17 23:22:14 +0300

    first commit

--- src/app/modules/admin/admin.module.ts
+++ src/app/modules/admin/admin.module.ts
@@ -2,12 +2,17 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+// Materials
+import { MatCardModule } from '@angular/material';
+import { MatIconModule } from '@angular/material';
+import { MatButtonModule } from '@angular/material';
 
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { AdminComponent } from './components/admin/admin.component';
 import { MyProjectComponent } from './components/my-project/my-project.component';
 import { NewEmployeeComponent } from './components/new-employee/new-employee.component';
 import { NewProjectComponent } from './components/new-project/new-project.component';
+import { TeamCardComponent } from './components/team-card/team-card.component';
 
 @NgModule({
     declarations: [
@@ -15,13 +20,17 @@ import { NewProjectComponent } from './components/new-project/new-project.compon
         AdminComponent,
         MyProjectComponent,
         NewEmployeeComponent,
-        NewProjectComponent
+        NewProjectComponent,
+        TeamCardComponent
     ],
     imports: [
         CommonModule,
         RouterModule,
         FormsModule,
-        ReactiveFormsModule
+        ReactiveFormsModule,
+        MatCardModule,
+        MatIconModule,
+        MatButtonModule
     ]
 })
 export class AdminModule {
