79ca457cbcdced847cd568c4c06f3879858b795c
Olga Kalatusha <Olga.Kalatusha@lanit-tercom.com>
2020-10-05 21:16:37 +0300

    minor fixes

--- src/app/modules/admin/components/new-project/new-project.component.ts
+++ src/app/modules/admin/components/new-project/new-project.component.ts
@@ -1,17 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
-import {teamCards, TeamCard} from './team-cards';
+import { teamCards, TeamCard } from './team-cards';
 
 @Component({
   selector: 'do-new-project',
   templateUrl: './new-project.component.html',
-  styleUrls: ['./new-project.component.scss']
+  styleUrls: [ './new-project.component.scss' ]
 })
-export class NewProjectComponent implements OnInit {
-  teams: TeamCard[] = teamCards;
+export class NewProjectComponent {
+  public teams: TeamCard[] = teamCards;
+
   constructor() { }
-
-  ngOnInit(): void {
-  }
-
 }
--- src/app/modules/admin/components/team-cards/team-card.component.ts
+++ src/app/modules/admin/components/team-cards/team-card.component.ts
@@ -1,14 +1,16 @@
-import {Component, OnInit, Input} from '@angular/core';
+import { Component, Input } from '@angular/core';
 
-import {TeamCard} from '../new-project/team-cards';
+import { TeamCard } from '../new-project/team-cards';
 
 @Component({
   selector: 'do-team-card',
   templateUrl: './team-card.component.html',
-  styleUrls: ['./team-card.component.scss']
+  styleUrls: [ './team-card.component.scss' ]
 })
 export class TeamCardComponent {
-  @Input() teamCard: TeamCard;
+  @Input()
+  public teamCard: TeamCard;
+
   constructor() { }
 
   public countLevels(members): string {
@@ -16,7 +18,7 @@ export class TeamCardComponent {
       levelsObj[member.level] = (levelsObj[member.level] || 0) + 1;
       return levelsObj;
     }, {});
-    return Object.keys(levels).map(level => `${levels[level]} ${level}`).join(', ');
+    return Object.keys(levels).map(level => `${ levels[level] } ${ level }`).join(', ');
   }
 
   public addMember(members): void {
