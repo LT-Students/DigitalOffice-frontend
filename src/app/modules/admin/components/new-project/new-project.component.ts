e3c9c66049b34831ade9a1e33204b515424173b3
MariyaSin <maria1991@list.ru>
2020-10-20 19:08:38 +0300

    added information form, added style for material input

--- src/app/modules/admin/components/new-project/new-project.component.ts
+++ src/app/modules/admin/components/new-project/new-project.component.ts
@@ -1,14 +1,20 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 
 import { teamCards, TeamCard } from './team-cards';
 
 @Component({
   selector: 'do-new-project',
   templateUrl: './new-project.component.html',
-  styleUrls: [ './new-project.component.scss' ]
+  styleUrls: ['./new-project.component.scss'],
 })
 export class NewProjectComponent {
   public teams: TeamCard[] = teamCards;
+  profileForm = new FormGroup({
+    projectName: new FormControl(''),
+    shortProjectName: new FormControl(''),
+  });
+  departments = ['one', 'two', 'three'];
 
-  constructor() { }
+  constructor() {}
 }
