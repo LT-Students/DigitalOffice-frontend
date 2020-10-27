6a5e1fb5f2eb1d6dbcfdfbd5a3f8d4208bfaf812
Anastassia Varabei <nastenkaok1984@gmail.com>
2020-10-28 01:51:42 +0300

    added ngIf for

--- src/app/modules/admin/components/new-member/new-member.component.ts
+++ src/app/modules/admin/components/new-member/new-member.component.ts
@@ -31,16 +31,17 @@ export class NewMemberComponent implements OnInit {
   constructor() {
   }
 
-  // public checkProductsCount(number, titles) {
-  //   cases = [2, 0, 1, 1, 1, 2];
-  //   return titles[ (number % 100 > 4 && number % 100 < 20) ? 2 : cases[(number % 10 < 5) ? number % 10 : 5] ];
-  // }
-  // let count = productsCount,
-  // result = checkProductsCount(count, ['проект', 'проекта', 'проектов']);
-  // }
-
-
   ngOnInit(): void {
+  }
+
+  getCountProjectsTitle() {
+    // tslint:disable-next-line:variable-name
+    function declOfNum(number, titles) {
+      const cases = [2, 0, 1, 1, 1, 2];
+      return titles[ (number % 100 > 4 && number % 100 < 20) ? 2 : cases[(number % 10 < 5) ? number % 10 : 5] ];
+    }
+
+    return declOfNum(this.newMember.projectsCount, ['проект', 'проекта', 'проектов']);
   }
 
 }
