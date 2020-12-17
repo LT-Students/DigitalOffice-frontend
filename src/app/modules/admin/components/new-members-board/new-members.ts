a2b0c6035eca7de082ae3add24dbefe5dc27ef0e
MariyaSin <maria1991@list.ru>
2020-12-17 15:16:57 +0300

    changed specialization and level key type for new-members

--- src/app/modules/admin/components/new-members-board/new-members.ts
+++ src/app/modules/admin/components/new-members-board/new-members.ts
@@ -1,23 +1,8 @@
-/* enum Level {
-  Junior = 'Junior',
-  Middle = 'Middle',
-  Senior = 'Senior',
-  Empty = '',
-}
-
-enum Specialization {
-  Frontend = 'Front-End Developer',
-  Backend = 'Backend-End Developer',
-  Manager = 'Product Manager',
-  Designer = 'UI/UX Designer',
-  Tester = 'QA Tester',
-  Empty = '',
-} */
 export interface NewMember {
   fullName: string;
   lead?: boolean;
   profileImgSrc: string;
-  specialization: string; //Specialization[];
+  specialization: string;
   projectsCount: number;
-  level: string; //Level;
+  level: string;
 }
