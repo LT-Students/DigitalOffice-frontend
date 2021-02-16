0b88a36b07b7a2ecb109243e14ad0d4da0eaf13d
MariyaSin <maria1991@list.ru>
2021-02-17 01:35:50 +0300

    added projects table component, changed my-project.component to project-card.component

--- src/app/modules/user/user.module.ts
+++ src/app/modules/user/user.module.ts
@@ -4,6 +4,7 @@ import { NgbDatepickerModule } from '@ng-bootstrap/ng-bootstrap';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppMaterialModule } from '../../app-material.module';
+import { AdminModule } from '../admin/admin.module';
 import { SharedModule } from '../shared/shared.module';
 import { TagsBlockComponent } from './components/tags-block/tags-block.component';
 import { DateDescComponent } from './components/date-desc/date-desc.component';
@@ -14,6 +15,7 @@ import { GradientGraphicsComponent } from './components/gradient-graphics/gradie
 import { UserTasksComponent } from './components/user-tasks/user-tasks.component';
 import { ProjectComponent } from './components/project/project.component';
 import { TaskComponent } from './components/project/task/task.component';
+import { ProjectsTableComponent } from './components/projects-table/projects-table.component';
 
 @NgModule({
   declarations: [
@@ -26,6 +28,7 @@ import { TaskComponent } from './components/project/task/task.component';
     UserTasksComponent,
     ProjectComponent,
     TaskComponent,
+    ProjectsTableComponent,
   ],
   imports: [
     CommonModule,
@@ -34,6 +37,7 @@ import { TaskComponent } from './components/project/task/task.component';
     AppMaterialModule,
     ReactiveFormsModule,
     FormsModule,
+    AdminModule,
   ],
   providers: [],
 })
