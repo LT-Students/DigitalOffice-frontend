fb8a52ca98d9ce5cc4b0128683ef578c5e2ef6c4
Anastassia Varabei <nastenkaok1984@gmail.com>
2021-02-21 16:12:05 +0300

    added component upload

---
+++ src/app/modules/admin/components/upload/upload.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'do-upload',
+  templateUrl: './upload.component.html',
+  styleUrls: ['./upload.component.scss'],
+})
+export class UploadComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
