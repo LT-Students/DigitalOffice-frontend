23dde833310f31d4d11d552a4ae288380b6d2f94
AlexLevus <33965367+AlexLevus@users.noreply.github.com>
2021-02-25 15:31:32 +0300

    Update login.component.ts

--- src/app/modules/auth/components/login/login.component.ts
+++ src/app/modules/auth/components/login/login.component.ts
@@ -29,7 +29,7 @@ export class LoginComponent implements OnInit {
     private formBuilder: FormBuilder
   ) {
     this.loginForm = this.formBuilder.group({
-      email: ['', [Validators.required, Validators.email]],
+      email: ['', [Validators.required]],
       password: ['', Validators.required],
     });
   }
