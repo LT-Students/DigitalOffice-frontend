e7a1400b0361ddd1a7bda7bbb79ca7a82f2e93bf
AlexLevus <Aleksandr.Levus@lanit-tercom.com>
2021-03-01 00:00:01 +0300

    Project refactoring

---
+++ src/app/data/store/project.store.ts
@@ -0,0 +1,110 @@
+import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+import { TimeManagementService } from '../services/time-management.service';
+import { ProjectService } from '../services/project.service';
+import { Project } from '../models/project';
+import { WorkTime } from '../models/work-time';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ProjectStore {
+  private projectsData: Project[] = [
+    {
+      id: '1',
+      name: 'Ромашка',
+      shortName: 'RO',
+      description: '13',
+      departmentId: '1',
+      isActive: true,
+      workTime: [
+        {
+          minutes: 60,
+          title: 'Developing',
+          description: 'desc1',
+          createdAt: new Date(),
+        },
+        {
+          minutes: 60,
+          title:
+            'Очень длинная задача, которую мне отдал мой коллега, а я не очень хотел ее делать и она занимает две строки',
+          description: 'desc2',
+          createdAt: new Date(),
+        },
+      ],
+    },
+    {
+      id: '2',
+      name: 'Рога и копыта',
+      shortName: 'RO',
+      description: '13',
+      departmentId: '1',
+      isActive: true,
+      workTime: [
+        {
+          minutes: 123,
+          title: 'Developing',
+          description: 'desc1',
+          createdAt: new Date(),
+        },
+      ],
+    },
+    {
+      id: '3',
+      name: 'Ещё один проект',
+      shortName: 'RO',
+      description: '13',
+      departmentId: '1',
+      isActive: true,
+      workTime: [
+        {
+          minutes: 123,
+          title: 'Документация',
+          description: 'desc1',
+          createdAt: new Date(),
+        },
+      ],
+    },
+  ];
+
+  private readonly _projects = new BehaviorSubject<Project[]>(
+    this.projectsData
+  );
+
+  readonly projects$ = this._projects.asObservable();
+
+  constructor(
+    private projectService: ProjectService,
+    private timeManagementService: TimeManagementService
+  ) {
+    // this.loadInitialData();
+  }
+
+  get projects(): Project[] {
+    return this._projects.getValue();
+  }
+  // TODO убрать Partial
+  addWorkTimeToProject(workTime: Partial<WorkTime>, projectId: string): void {
+    // вызываем бэкенд метод, для создания WorkTime
+    // проверить на работоспособность, раньше проверка была такая
+    // project.id === id + 1 ???
+    this.projects.forEach((project: Project) => {
+      if (project.id === projectId) {
+        project.workTime.push(workTime);
+        this._projects.next(this.projects.slice());
+      }
+    });
+  }
+
+  /*  private loadInitialData(): void {
+    this.projectService.getProjects().subscribe((projects) => {});
+    forkJoin([
+      this.projectService.getProjects(),
+      // сюда прокинуть userId, startTime, endTime
+      this.timeManagementService.getUserWorkTimes(),
+    ]).subscribe((res) => {
+      const [projects, tasks] = res;
+      // тут заполнить проекты тасками
+    });
+  }*/
+}
