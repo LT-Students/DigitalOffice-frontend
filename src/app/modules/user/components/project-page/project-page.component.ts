9a89f0424c59ca0c65fdcdf6fce3b7170c481317
CYX4Rb <70616505+CYX4Rb@users.noreply.github.com>
2021-03-02 14:54:36 +0300

    review

--- src/app/modules/user/components/project-page/project-page.component.ts
+++ src/app/modules/user/components/project-page/project-page.component.ts
@@ -1,26 +1,114 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-project-page',
+  selector: 'do-project-page',
   templateUrl: './project-page.component.html',
-  styleUrls: ['./project-page.component.scss']
+  styleUrls: ['./project-page.component.scss'],
 })
-
 export class ProjectPageComponent implements OnInit {
-  titleProject: string = 'Меркурий'
-  descriptionProject: string = 'Депортамент цифровых решений'
-  detailsProject: Array<string> = ['Роскосмос', 'В работе', '26 октября 2019', '233 дня', '19 человек']
-  smallPhotos: Array<string> = [
-    '../../../../../assets/images/smallPhoto/Ellipse90.png', 
-    '../../../../../assets/images/smallPhoto/Ellipse91.png', 
-    '../../../../../assets/images/smallPhoto/Ellipse92.png', 
-    '../../../../../assets/images/smallPhoto/Ellipse93.png', 
-    '../../../../../assets/images/smallPhoto/Ellipse94.png', 
-    '../../../../../assets/images/smallPhoto/Ellipse95.png', 
-    '../../../../../assets/images/smallPhoto/Ellipse96.png'
-  ]
+  projectPage = {
+    title: 'Меркурий',
+    description: 'Департамент цифровых решений',
 
-  constructor() { }
-  ngOnInit(): void {
-  }
+    details: [
+      { header: 'Заказчик', text: 'Роскосмос' },
+      { header: 'статус', text: 'В работе ' },
+      { header: 'Запуск проекта', text: '20 октября 2019' },
+      { header: 'Проект длится', text: '233 дня' },
+      { header: 'Команда', text: '19 человек' },
+    ],
+
+    teamProgress: {
+      allTime: '2858',
+      lastTime: '264',
+      lastMonth: 'Июнь 2020',
+    },
+
+    members: [
+      {
+        id: '88',
+        firstName: 'Eva',
+        lastName: 'Tener',
+        smallPhoto: 'assets/images/smallPhoto/Ellipse88.png',
+        role: 'Product Manager',
+        progress: { allTime: '295', lastTime: '40', lastMonth: 'Июнь 2020' },
+      },
+
+      {
+        id: '89',
+        firstName: 'Jerry',
+        lastName: 'Fenster',
+        smallPhoto: 'assets/images/smallPhoto/Ellipse89.png',
+        role: 'TeamLead',
+        progress: { allTime: '295', lastTime: '40', lastMonth: 'Июнь 2020' },
+      },
+
+      {
+        id: '90',
+        firstName: '',
+        lastName: '',
+        smallPhoto: 'assets/images/smallPhoto/Ellipse90.png',
+        role: '',
+        progress: { allTime: '295', lastTime: '40', lastMonth: 'Июнь 2020' },
+      },
+
+      {
+        id: '91',
+        firstName: '',
+        lastName: '',
+        smallPhoto: 'assets/images/smallPhoto/Ellipse91.png',
+        role: '',
+        progress: { allTime: '295', lastTime: '40', lastMonth: 'Июнь 2020' },
+      },
+
+      {
+        id: '92',
+        firstName: '',
+        lastName: '',
+        smallPhoto: 'assets/images/smallPhoto/Ellipse92.png',
+        role: '',
+        progress: { allTime: '295', lastTime: '40', lastMonth: 'Июнь 2020' },
+      },
+
+      {
+        id: '93',
+        firstName: '',
+        lastName: '',
+        smallPhoto: 'assets/images/smallPhoto/Ellipse93.png',
+        role: '',
+        progress: { allTime: '295', lastTime: '40', lastMonth: 'Июнь 2020' },
+      },
+
+      {
+        id: '94',
+        firstName: '',
+        lastName: '',
+        smallPhoto: 'assets/images/smallPhoto/Ellipse94.png',
+        role: '',
+        progress: { allTime: '295', lastTime: '40', lastMonth: 'Июнь 2020' },
+      },
+
+      {
+        id: '95',
+        firstName: '',
+        lastName: '',
+        smallPhoto: 'assets/images/smallPhoto/Ellipse95.png',
+        role: '',
+        progress: { allTime: '295', lastTime: '40', lastMonth: 'Июнь 2020' },
+      },
+
+      {
+        id: '96',
+        firstName: '',
+        lastName: '',
+        smallPhoto: 'assets/images/smallPhoto/Ellipse96.png',
+        role: '',
+        progress: { allTime: '295', lastTime: '40', lastMonth: 'Июнь 2020' },
+      },
+    ],
+  };
+
+  userId = '89';
+  constructor() {}
+  ngOnInit(): void {}
 }
