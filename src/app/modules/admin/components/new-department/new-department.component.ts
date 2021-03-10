9a3bede68309202c889a6d87e429394f31331fb0
MariyaSin <maria1991@list.ru>
2021-03-11 02:47:20 +0300

    added department component

--- src/app/modules/admin/components/new-department/new-department.component.ts
+++ src/app/modules/admin/components/new-department/new-department.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
 import { MatDialog } from '@angular/material/dialog';
 import { HttpErrorResponse } from '@angular/common/http';
