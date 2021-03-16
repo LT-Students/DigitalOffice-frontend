a5503b8ea3c21e22c4d4663a6cfe1e0d10012755
CYX4Rb <70616505+CYX4Rb@users.noreply.github.com>
2021-03-16 17:21:08 +0300

    added input

--- src/app/modules/user/components/project/project.component.ts
+++ src/app/modules/user/components/project/project.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit, Input } from '@angular/core';
-
 import { Project } from '../../../../data/models/project';
 
 @Component({
@@ -10,6 +9,9 @@ import { Project } from '../../../../data/models/project';
 export class ProjectComponent implements OnInit {
   @Input() project: Project;
 
+  deleteWorkTime(title: string) {
+    this.project.workTime.filter((x) => x.title !== title); // как сохранить контекст вот здесь)))
+  }
   constructor() {}
 
   ngOnInit() {}
--- src/app/modules/user/components/project/task/task.component.ts
+++ src/app/modules/user/components/project/task/task.component.ts
@@ -1,6 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
 
-import { ITask } from '../../../../../core/interfaces/task.interface';
+/* import { ITask } from '../../../../../core/interfaces/task.interface'; */
 import { WorkTime } from '../../../../../data/models/work-time';
 
 @Component({
@@ -9,11 +9,22 @@ import { WorkTime } from '../../../../../data/models/work-time';
   styleUrls: ['./task.component.scss'],
 })
 export class TaskComponent implements OnInit {
+  isInput = false;
   @Input() workTime: WorkTime;
   hours = 0;
   minutes = 0;
+  @Input() delete: any;
 
   constructor() {}
+
+  changeInput() {
+    console.log('input');
+    this.isInput = !this.isInput;
+  }
+
+  changeTitle(value: string) {
+    this.workTime.title = value;
+  }
 
   ngOnInit() {
     this.hours = Math.floor(this.workTime.minutes / 60);
