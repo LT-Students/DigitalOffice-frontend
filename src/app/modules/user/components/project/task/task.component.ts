ca68dd08bde8ed5965e899ef9c2dc78983b932da
CYX4Rb <70616505+CYX4Rb@users.noreply.github.com>
2021-03-17 19:53:10 +0300

    fix function

--- src/app/modules/user/components/project/task/task.component.ts
+++ src/app/modules/user/components/project/task/task.component.ts
@@ -17,12 +17,12 @@ export class TaskComponent implements OnInit {
 
   constructor(private projectStore: ProjectStore) {}
 
-  changeInput(): Function {
-    return () => (this.isInput = !this.isInput);
+  changeInput() {
+    this.isInput = !this.isInput;
   }
 
-  changeTitle(value: string): Function {
-    return () => (this.task.description = value);
+  changeTitle(value: string) {
+    this.task.description = value;
   }
 
   deleteTask(projectId: string, taskId: string): Function {
