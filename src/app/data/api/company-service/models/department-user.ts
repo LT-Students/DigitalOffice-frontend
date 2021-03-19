ab62fa65c10de847878b95463b68f03d607a5f3f
AlexLevus <Aleksandr.Levus@lanit-tercom.com>
2021-03-19 09:02:45 +0300

    Update CompanyService api

---
+++ src/app/data/api/company-service/models/department-user.ts
@@ -0,0 +1,17 @@
+/* tslint:disable */
+/* eslint-disable */
+
+/**
+ * User data of specific department.
+ */
+export interface DepartmentUser {
+  /**
+   * Specific position id.
+   */
+  positionId?: string;
+
+  /**
+   * Specific users id.
+   */
+  userId?: string;
+}
