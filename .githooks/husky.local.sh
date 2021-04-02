417e3c30e6669f90f3429b836dc7faf40e026ebd
NikGurev <nickita.gurev@yandex.ru>
2021-04-02 12:20:51 +0300

    hooks removed to external folder to share with everyone

---
+++ .githooks/husky.local.sh
@@ -0,0 +1,6 @@
+# Created by Husky v4.3.0 (https://github.com/typicode/husky#readme)
+#   At: 19.03.2021, 17:24:00
+#   From: undefined (https://github.com/typicode/husky#readme)
+
+packageManager=npm
+cd "."
