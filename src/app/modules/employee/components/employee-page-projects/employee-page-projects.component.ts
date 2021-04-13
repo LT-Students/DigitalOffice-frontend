ba01a45387a5f35c5debe982e36e2992d04549cb
romantsinevich <braid98@yandex.ru>
2021-04-13 13:28:52 +0300

    Move initialization to constructor

--- src/app/modules/employee/components/employee-page-projects/employee-page-projects.component.ts
+++ src/app/modules/employee/components/employee-page-projects/employee-page-projects.component.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { Project } from '@data/models/project';
+
+interface UserProject extends Project {
+  role: string;
+  startedAt: Date;
+  endedAt?: Date;
+}
 
 @Component({
   selector: 'do-employee-page-projects',
@@ -6,64 +13,86 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./employee-page-projects.component.scss'],
 })
 export class EmployeePageProjectsComponent implements OnInit {
-  public project = {
-    id: '0',
-    name: 'Алиса в стране Чудес',
-    company: 'Iponkin Brothers',
-    description:
-      'Разработка и внедрение  системы дополненной реальности под PS VR',
-    role: 'Product Manager (Middle)',
-    startedAt: new Date(2020, 0, 20),
-    endedAt: '',
-  };
+  public userProjects: UserProject[];
+  public activeProjects: UserProject[] = [];
+  public participatedProjects: UserProject[] = [];
 
-  public activeProjects = [];
-  public participatedProjects = [];
-
-  public status = true;
-
-  public pluralForm = {
-    roditelny: {
-      one: '# проект',
-      few: '# проекта',
-      other: '# проектов',
-    },
-    predlozhny: {
-      one: '# проекте',
-      other: '# проектах',
-    },
-  };
-
-  public groups = [
-    {
-      name: 'В работе',
-      projects: this.activeProjects,
-      expanded: false,
-      plural: this.pluralForm.roditelny,
-    },
-    {
-      name: 'Участвовал в',
-      projects: this.participatedProjects,
-      expanded: false,
-      plural: this.pluralForm.predlozhny,
-    },
-  ];
+  public pluralForm;
+  public groups;
 
   constructor() {
-    this.activeProjects.push(this.project, this.project);
-    this.participatedProjects.push(
-      this.project,
-      this.project,
-      this.project,
-      this.project,
-      this.project,
-      this.project
-    );
+    this.userProjects = this._getUserProjects();
+
+    this.userProjects.forEach((project) => {
+      if (project.isActive) {
+        this.activeProjects.push(project);
+      } else {
+        this.participatedProjects.push(project);
+      }
+    });
+
+    this.pluralForm = {
+      roditelny: {
+        one: '# проект',
+        few: '# проекта',
+        other: '# проектов',
+      },
+      predlozhny: {
+        one: '# проекте',
+        other: '# проектах',
+      },
+    };
+
+    this.groups = [
+      {
+        name: 'В работе',
+        projects: this.activeProjects,
+        expanded: false,
+        plural: this.pluralForm.roditelny,
+      },
+      {
+        name: 'Участвовал в',
+        projects: this.participatedProjects,
+        expanded: false,
+        plural: this.pluralForm.predlozhny,
+      },
+    ];
   }
 
   ngOnInit(): void {}
 
-  expandProjects(group) {
+  private _getUserProjects(): UserProject[] {
+    const activeProject = {
+      id: 'random id',
+      name: 'Алиса в стране Чудес',
+      shortName: 'something',
+      description:
+        'Разработка и внедрение  системы дополненной реальности под PS VR',
+      departmentId: 'random id',
+      isActive: true,
+      consumer: { name: 'Iponkin Brothers', description: 'lalalal' },
+      role: 'Product Manager (Middle)',
+      startedAt: new Date(2020, 0, 20),
+    };
+
+    const closedProject = {
+      ...activeProject,
+      isActive: false,
+      endedAt: new Date(2021, 5, 5),
+    };
+
+    return [
+      activeProject,
+      activeProject,
+      closedProject,
+      closedProject,
+      closedProject,
+      closedProject,
+      closedProject,
+    ];
+  }
+
+  public expandProjects(group): void {
     group.expanded = !group.expanded;
   }
 }
