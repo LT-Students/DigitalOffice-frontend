b230269b1817d055fd3b067c4ec4ea3f56522ba9
romantsinevich <braid98@yandex.ru>
2021-04-26 00:06:52 +0300

    Add routing for breadcrumbs

--- src/app/modules/employee/components/breadcrumbs/breadcrumbs.component.ts
+++ src/app/modules/employee/components/breadcrumbs/breadcrumbs.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
     selector: 'do-breadcrumbs',
@@ -6,15 +7,22 @@ import { Component, OnInit } from '@angular/core';
     styleUrls: ['./breadcrumbs.component.scss'],
 })
 export class BreadcrumbsComponent implements OnInit {
+    @Input() department: string = 'Департамент Цифровых Решений';
+    @Input() employeeName: string = 'Ангелина Иванова';
+
     paths: any[];
 
-    constructor() {
+    constructor(private router: Router) {
         this.paths = [
-            { label: 'Сотрудники', route: 'yo' },
-            { label: 'Департамент Цифровых Решений', route: 'yo' },
-            { label: 'Ангелина Иванова', route: 'yo' },
+            { label: 'Сотрудники', route: 'user/attendance' },
+            { label: this.department, route: 'user/attendance' },
+            { label: this.employeeName, },
         ];
     }
 
     ngOnInit(): void {}
+
+    onClickBreadcrumbs(route: string) {
+        this.router.navigate([route]);
+    }
 }
