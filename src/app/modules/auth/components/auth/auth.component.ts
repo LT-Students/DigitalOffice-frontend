2185f83da03feec0af3988477ad4ef9cf9214941
romantsinevich <braid98@yandex.ru>
2021-05-01 18:34:55 +0300

    Add parallax effect to waves

--- src/app/modules/auth/components/auth/auth.component.ts
+++ src/app/modules/auth/components/auth/auth.component.ts
@@ -1,12 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, HostListener, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'do-auth',
-  templateUrl: './auth.component.html',
-  styleUrls: ['./auth.component.scss'],
+    selector: 'do-auth',
+    templateUrl: './auth.component.html',
+    styleUrls: ['./auth.component.scss'],
 })
 export class AuthComponent implements OnInit {
-  constructor() {}
+    constructor(private elem: ElementRef) {}
 
-  ngOnInit(): void {}
+    ngOnInit(): void {}
+
+    @HostListener('mousemove', ['$event'])
+    parallax(e) {
+        this.elem.nativeElement
+            .querySelectorAll('.waves-container')
+            .forEach((wave) => {
+                const speedX = +wave.getAttribute('data-speedX');
+                const speedY = +wave.getAttribute('data-speedY');
+
+                const x = (window.innerWidth - e.pageX * speedX) / 100;
+                const y = (window.innerHeight - e.pageY * speedY) / 100;
+
+                wave.style.transform = `translate(${x}px, ${y}px)`;
+            });
+    }
 }
