d14e69a4a514d8577d55947188bd253155a59ac2
NikGurev <nickita.gurev@yandex.ru>
2021-05-05 18:19:41 +0300

    added new project layout

--- src/app/modules/admin/components/new-project/new-project.component.ts
+++ src/app/modules/admin/components/new-project/new-project.component.ts
@@ -1,13 +1,12 @@
 import { Component, OnInit } from '@angular/core';
-import {
-  FormControl,
-  FormGroup,
-  Validators,
-  FormBuilder,
-} from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { MatDialog } from '@angular/material/dialog';
 import { NewMembersBoardComponent } from '../new-members-board/new-members-board.component';
-import { teamCards, TeamCard } from './team-cards';
+import { TeamCard, teamCards } from './team-cards';
+import { DepartmentInfo } from '@data/api/user-service/models/department-info';
+import { ProjectStatus } from '@app/models/project-status';
+import { ProjectStatusType } from '@data/api/project-service/models/project-status-type';
+import { ProjectService } from '@app/services/project.service';
 
 @Component({
   selector: 'do-new-project',
@@ -17,7 +16,9 @@ import { teamCards, TeamCard } from './team-cards';
 export class NewProjectComponent implements OnInit {
   public projectForm: FormGroup;
   public teams: TeamCard[] = teamCards;
-  public departments = ['one', 'two', 'three'];
+  // TODO: REPLACE WITH API
+  public departments: DepartmentInfo[];
+  public statuses: ProjectStatus[];
   public team = [
     {
       name: 'Olya',
@@ -29,7 +30,32 @@ export class NewProjectComponent implements OnInit {
     },
   ];
 
-  constructor(public dialog: MatDialog, private formBuilder: FormBuilder) {}
+  constructor(public dialog: MatDialog,
+              private formBuilder: FormBuilder,
+              private _projectService: ProjectService) {
+    this.statuses = [
+        new ProjectStatus(ProjectStatusType.Active),
+        new ProjectStatus(ProjectStatusType.Closed),
+        new ProjectStatus(ProjectStatusType.Suspend),
+    ]
+    this.departments = [
+      {
+        id: 'cc289eba-ada8-11eb-8529-0242ac130003',
+        name: 'Департамент Цифровых решений',
+        startWorkingAt: '14/14/21'
+      },
+      {
+        id: 'f3a25346-ada8-11eb-8529-0242ac130003',
+        name: 'Департамент тестовых решений',
+        startWorkingAt: '14/14/21'
+      },
+      {
+        id: 'f6fd15ee-ada8-11eb-8529-0242ac130003',
+        name: 'Департамент медицинских решений',
+        startWorkingAt: '14/14/21'
+      },
+    ];
+}
 
   ngOnInit(): void {
     this.projectForm = this.formBuilder.group({
@@ -54,7 +80,8 @@ export class NewProjectComponent implements OnInit {
   }
 
   public createProject(): void {}
-  public showTeam(): void {}
+  public onAddTeamClick(): void {}
+  public showProjectTeam(): void {}
   public saveDraft(): void {
     console.log('Сохранить черновик');
   }
