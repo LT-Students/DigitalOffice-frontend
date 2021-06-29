a6252947db85bd0ff1c0d8e682bde2134edebbaa
romantsinevich <braid98@yandex.ru>
2021-06-29 12:39:23 +0300

    Remove nav link

--- src/app/modules/admin/components/new-role/new-role.component.ts
+++ src/app/modules/admin/components/new-role/new-role.component.ts
@@ -9,8 +9,8 @@ import { MatSnackBar } from '@angular/material/snack-bar';
 import { switchMap } from 'rxjs/operators';
 import { UsersResponse } from '@data/api/user-service/models/users-response';
 import { HttpErrorResponse } from '@angular/common/http';
-import { UserSearchComponent } from '../new-project/modals/user-search/user-search.component';
 import { RightsApiService } from '@data/api/rights-service/services/rights-api.service';
+import { UserSearchComponent } from '../new-project/modals/user-search/user-search.component';
 
 @Component({
 	selector: 'do-new-role',
@@ -18,20 +18,12 @@ import { RightsApiService } from '@data/api/rights-service/services/rights-api.s
 	styleUrls: ['./new-role.component.scss'],
 })
 export class NewRoleComponent implements OnInit {
-	public directors: UserInfo[] = [];
-	private getDirectorsSubscription: Subscription;
 	public rights = [];
 
-	public roleForm = new FormGroup({
-		name: new FormControl(null, [Validators.required]),
-		description: new FormControl(null, [Validators.required]),
-		directorId: new FormControl(null, [Validators.required]),
-	});
+	public roleForm: FormGroup;
 
 	constructor(
 		public rightsApiService: RightsApiService,
-		public userApiService: UserApiService,
-		public departmentApiService: DepartmentApiService,
 		private dialogRef: MatDialogRef<UserSearchComponent>,
 		private formBuilder: FormBuilder,
 		private snackBar: MatSnackBar
@@ -42,41 +34,36 @@ export class NewRoleComponent implements OnInit {
 
 		this.roleForm = this.formBuilder.group({
 			name: ['', [Validators.required, Validators.minLength(2), Validators.maxLength(100)]],
+			description: ['', [Validators.required, Validators.minLength(2), Validators.maxLength(100)]],
+      rights: [[], [Validators.required]],
 		});
 	}
 
 	getRights(): void {
 		this.rightsApiService.getRightsList().subscribe((rights) => (this.rights = rights));
-		// this.getDirectorsSubscription = this.userApiService
-		// 	.findUsers({
-		// 		skipCount: 0,
-		// 		takeCount: 50,
-		// 	})
-		// 	.pipe(switchMap((usersResponse: UsersResponse) => of(usersResponse.users)))
-		// 	.subscribe((data: UserInfo[]) => (this.directors = data));
 	}
 
 	postRole(): void {
-		this.departmentApiService
-			.addDepartment({
-				body: {
-					info: {
-						name: this.roleForm.controls['name'].value,
-						description: this.roleForm.controls['description'].value,
-						directorUserId: this.roleForm.controls['directorId'].value,
-					},
-				},
-			})
-			.subscribe(
-				(res) => {
-					this.snackBar.open('New role added successfully', 'done', {
-						duration: 3000,
-					});
-				},
-				(error: HttpErrorResponse) => {
-					this.snackBar.open(error.error.Message, 'accept');
-					throw error;
-				}
-			);
+		// this.departmentApiService
+		// 	.addDepartment({
+		// 		body: {
+		// 			info: {
+		// 				name: this.roleForm.controls['name'].value,
+		// 				description: this.roleForm.controls['description'].value,
+		// 				directorUserId: this.roleForm.controls['directorId'].value,
+		// 			},
+		// 		},
+		// 	})
+		// 	.subscribe(
+		// 		(res) => {
+		// 			this.snackBar.open('New role added successfully', 'done', {
+		// 				duration: 3000,
+		// 			});
+		// 		},
+		// 		(error: HttpErrorResponse) => {
+		// 			this.snackBar.open(error.error.Message, 'accept');
+		// 			throw error;
+		// 		}
+		// 	);
 	}
 }
