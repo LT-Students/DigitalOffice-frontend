07710fe109ce63471c53c9def058e5e0cf502604
romantsinevich <braid98@yandex.ru>
2021-07-01 13:35:32 +0300

    Create installer module

---
+++ src/app/modules/installer/installer.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { WizardComponent } from './components/wizard/wizard.component';
+
+
+
+@NgModule({
+  declarations: [WizardComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class InstallerModule { }
