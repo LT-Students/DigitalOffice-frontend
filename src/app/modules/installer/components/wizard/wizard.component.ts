1d84b02926ccac045b90b8dbc1dba3bd0fa7f7be
romantsinevich <braid98@yandex.ru>
2021-07-02 16:11:31 +0300

    Create form group for each step

--- src/app/modules/installer/components/wizard/wizard.component.ts
+++ src/app/modules/installer/components/wizard/wizard.component.ts
@@ -7,18 +7,36 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 	styleUrls: ['./wizard.component.scss'],
 })
 export class WizardComponent implements OnInit {
-	firstFormGroup: FormGroup;
-	secondFormGroup: FormGroup;
-	isEditable = false;
+	companyForm: FormGroup;
+	adminForm: FormGroup;
+	smtpForm: FormGroup;
 
 	constructor(private _formBuilder: FormBuilder) {}
 
 	ngOnInit() {
-		this.firstFormGroup = this._formBuilder.group({
-			firstCtrl: ['', Validators.required],
+		this.companyForm = this._formBuilder.group({
+			companyName: ['', Validators.required],
+			portalName: ['', Validators.required],
+			siteUrl: ['', Validators.required],
 		});
-		this.secondFormGroup = this._formBuilder.group({
-			secondCtrl: ['', Validators.required],
+		this.adminForm = this._formBuilder.group({
+			firstName: ['', Validators.required],
+			lastName: ['', Validators.required],
+			middleName: ['', Validators.required],
+			email: ['', Validators.required],
+			login: ['', Validators.required],
+			password: ['', Validators.required],
 		});
+		this.smtpForm = this._formBuilder.group({
+			host: ['', Validators.required],
+			port: ['', Validators.required],
+			enableSSL: ['', Validators.required],
+			email: ['', Validators.required],
+			password: ['', Validators.required],
+		});
+	}
+
+	submitForm() {
+		console.log(this.companyForm.value, this.adminForm.value, this.smtpForm.value);
 	}
 }
