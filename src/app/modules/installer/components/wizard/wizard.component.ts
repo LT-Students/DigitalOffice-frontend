01a635c421f213addcc69d652e081a90b5e22801
romantsinevich <braid98@yandex.ru>
2021-07-02 16:18:54 +0300

    Create form group for each step

--- src/app/modules/installer/components/wizard/wizard.component.ts
+++ src/app/modules/installer/components/wizard/wizard.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { CompanyApiService } from '@data/api/company-service/services/company-api.service';
 
 @Component({
 	selector: 'do-wizard',
@@ -11,7 +12,7 @@ export class WizardComponent implements OnInit {
 	adminForm: FormGroup;
 	smtpForm: FormGroup;
 
-	constructor(private _formBuilder: FormBuilder) {}
+	constructor(private _formBuilder: FormBuilder, private companyApiService: CompanyApiService) {}
 
 	ngOnInit() {
 		this.companyForm = this._formBuilder.group({
@@ -22,7 +23,7 @@ export class WizardComponent implements OnInit {
 		this.adminForm = this._formBuilder.group({
 			firstName: ['', Validators.required],
 			lastName: ['', Validators.required],
-			middleName: ['', Validators.required],
+			middleName: [''],
 			email: ['', Validators.required],
 			login: ['', Validators.required],
 			password: ['', Validators.required],
@@ -30,7 +31,7 @@ export class WizardComponent implements OnInit {
 		this.smtpForm = this._formBuilder.group({
 			host: ['', Validators.required],
 			port: ['', Validators.required],
-			enableSSL: ['', Validators.required],
+			enableSsl: ['', Validators.required],
 			email: ['', Validators.required],
 			password: ['', Validators.required],
 		});
@@ -38,5 +39,27 @@ export class WizardComponent implements OnInit {
 
 	submitForm() {
 		console.log(this.companyForm.value, this.adminForm.value, this.smtpForm.value);
+		this.companyApiService.createCompany({
+			body: {
+				companyName: this.companyForm.get('companyName').value,
+				portalName: this.companyForm.get('portalName').value,
+				siteUrl: this.companyForm.get('siteUrl').value,
+				adminInfo: {
+					firstName: this.adminForm.get('firstName').value,
+					lastName: this.adminForm.get('lastName').value,
+					middleName: this.adminForm.get('middleName').value,
+					email: this.adminForm.get('email').value,
+					login: this.adminForm.get('login').value,
+					password: this.adminForm.get('password').value,
+				},
+				sMTP: {
+					host: this.smtpForm.get('host').value,
+					port: this.smtpForm.get('port').value,
+					email: this.smtpForm.get('email').value,
+					password: this.smtpForm.get('password').value,
+					enableSsl: this.smtpForm.get('enableSsl').value,
+				}
+			},
+		});
 	}
 }
