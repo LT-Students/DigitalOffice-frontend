556113c7b3701f2f1d3a76e97c48e08db805c860
romantsinevich <braid98@yandex.ru>
2021-07-04 19:50:30 +0300

    Add method to create company from installer

--- src/app/modules/installer/components/wizard/wizard.component.ts
+++ src/app/modules/installer/components/wizard/wizard.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { CompanyApiService } from '@data/api/company-service/services/company-api.service';
+import { Router } from '@angular/router';
 
 @Component({
 	selector: 'do-wizard',
@@ -12,7 +13,7 @@ export class WizardComponent implements OnInit {
 	adminForm: FormGroup;
 	smtpForm: FormGroup;
 
-	constructor(private _formBuilder: FormBuilder, private companyApiService: CompanyApiService) {}
+	constructor(private _formBuilder: FormBuilder, private companyApiService: CompanyApiService, private router: Router) {}
 
 	ngOnInit() {
 		this.companyForm = this._formBuilder.group({
@@ -38,28 +39,34 @@ export class WizardComponent implements OnInit {
 	}
 
 	submitForm() {
-		console.log(this.companyForm.value, this.adminForm.value, this.smtpForm.value);
-		this.companyApiService.createCompany({
-			body: {
-				companyName: this.companyForm.get('companyName').value,
-				portalName: this.companyForm.get('portalName').value,
-				siteUrl: this.companyForm.get('siteUrl').value,
-				adminInfo: {
-					firstName: this.adminForm.get('firstName').value,
-					lastName: this.adminForm.get('lastName').value,
-					middleName: this.adminForm.get('middleName').value,
-					email: this.adminForm.get('email').value,
-					login: this.adminForm.get('login').value,
-					password: this.adminForm.get('password').value,
+		this.companyApiService
+			.createCompany({
+				body: {
+					companyName: this.companyForm.get('companyName').value,
+					portalName: this.companyForm.get('portalName').value,
+					siteUrl: this.companyForm.get('siteUrl').value,
+					adminInfo: {
+						firstName: this.adminForm.get('firstName').value,
+						lastName: this.adminForm.get('lastName').value,
+						middleName: this.adminForm.get('middleName').value,
+						email: this.adminForm.get('email').value,
+						login: this.adminForm.get('login').value,
+						password: this.adminForm.get('password').value,
+					},
+					sMTP: {
+						host: this.smtpForm.get('host').value,
+						port: this.smtpForm.get('port').value,
+						email: this.smtpForm.get('email').value,
+						password: this.smtpForm.get('password').value,
+						enableSsl: this.smtpForm.get('enableSsl').value,
+					},
 				},
-				sMTP: {
-					host: this.smtpForm.get('host').value,
-					port: this.smtpForm.get('port').value,
-					email: this.smtpForm.get('email').value,
-					password: this.smtpForm.get('password').value,
-					enableSsl: this.smtpForm.get('enableSsl').value,
-				}
-			},
-		});
+			})
+			.subscribe(
+				(result) => {
+					this.router.navigate(['/auth/login']);
+				},
+				(error) => console.log(error)
+			);
 	}
 }
