8cf792ecd1f65b8d6bef0d6b4df30073369c5d6a
romantsinevich <braid98@yandex.ru>
2021-07-08 18:51:23 +0300

    Update new project component

--- src/app/modules/admin/components/new-project/new-project.component.ts
+++ src/app/modules/admin/components/new-project/new-project.component.ts
@@ -2,13 +2,14 @@ import { ComponentType } from '@angular/cdk/overlay';
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { MatDialog } from '@angular/material/dialog';
-import { UserSearchComponent } from './modals/user-search/user-search.component';
 import { DepartmentInfo } from '@data/api/user-service/models/department-info';
 import { ProjectStatus } from '@app/models/project-status';
 import { ProjectStatusType } from '@data/api/project-service/models/project-status-type';
 import { ProjectService } from '@app/services/project.service';
-import { WorkFlowMode } from '../../../employee/employee-page.component';
 import { ModalService, ModalType, UserSearchModalConfig } from '@app/services/modal.service';
+import { NetService } from '@app/services/net.service';
+import { WorkFlowMode } from '../../../employee/employee-page.component';
+import { UserSearchComponent } from './modals/user-search/user-search.component';
 import { DeleteDirectionComponent, ModalApprovalConfig } from './modals/delete-direction/delete-direction.component';
 import { Team, teamCards, TeamMember } from './team-cards';
 
@@ -19,26 +20,17 @@ import { Team, teamCards, TeamMember } from './team-cards';
 })
 export class NewProjectComponent implements OnInit {
 	public projectForm: FormGroup;
-	public teams: Team[] = teamCards;
+	public teams: Team[];
 	// TODO: REPLACE WITH API
 	public departments: DepartmentInfo[];
 	public statuses: ProjectStatus[];
-	public team = [
-		{
-			name: 'Olya',
-			profileImgSrc: '',
-		},
-		{
-			name: 'Slava',
-			profileImgSrc: '',
-		},
-	];
 
 	constructor(
 		public dialog: MatDialog,
 		private formBuilder: FormBuilder,
 		private _projectService: ProjectService,
-		private _modalService: ModalService
+		private _modalService: ModalService,
+		private _netService: NetService
 	) {
 		this.statuses = [
 			new ProjectStatus(ProjectStatusType.Active),
@@ -62,32 +54,53 @@ export class NewProjectComponent implements OnInit {
 				startWorkingAt: '14/14/21',
 			},
 		];
-		this.teams = teamCards;
+		this.teams = [];
 	}
 
 	ngOnInit(): void {
 		this.projectForm = this.formBuilder.group({
 			name: ['', [Validators.required, Validators.maxLength(80)]],
-			shortName: ['', [Validators.required, Validators.maxLength(32)]],
-			departments: ['', [Validators.required, Validators.maxLength(32)]],
 			description: ['', [Validators.required, Validators.maxLength(500)]],
-			checkControl: ['', [Validators.required]],
-			additionInfo: [''],
-			department: [''],
+			departmentId: [''],
 			customer: [''],
 			status: [''],
-			picker: [''],
+			// shortName: ['', [Validators.required, Validators.maxLength(32)]],
+			// departments: ['', [Validators.required, Validators.maxLength(32)]],
+			// checkControl: ['', [Validators.required]],
+			// additionInfo: [''],
+			// picker: [''],
 		});
 
-		this.teams.forEach((team: Team) => this._sortLeads(team));
+		this._getDepartments();
+
+		// this.teams.forEach((team: Team) => this._sortLeads(team));
+	}
+
+	private _getDepartments(): void {
+		this._netService.getDepartmentsList().subscribe(
+			(data: DepartmentInfo[]) => {
+				this.departments = data;
+			},
+			(error) => console.log(error)
+		);
 	}
 
 	public addMember(): void {
 		const modalData: UserSearchModalConfig = { mode: WorkFlowMode.ADD };
-		this._modalService.openModal(UserSearchComponent, modalData);
+		const dialogRef = this._modalService.openModal(UserSearchComponent, modalData);
+		dialogRef.afterClosed().subscribe((result) => {
+			const newTeam: Team = {
+				name: result.role as string,
+				members: result.users,
+			}
+			this.teams.push(newTeam);
+		})
 	}
 
-	public createProject(): void {}
+	public createProject(): void {
+		console.log(this.projectForm.value);
+	}
+
 	public onAddTeamClick(): void {
 		this.addMember();
 	}
@@ -105,7 +118,7 @@ export class NewProjectComponent implements OnInit {
 	}
 
 	public totalMembersCount(): number {
-		return this.teams.map((team: Team) => team.members.length).reduce((sum: number, teamTotalNumber) => sum + teamTotalNumber);
+		return this.teams.map((team: Team) => team.members.length).reduce((sum: number, teamTotalNumber) => sum + teamTotalNumber, 0);
 	}
 
 	private _getAllMembers(): TeamMember[] {
