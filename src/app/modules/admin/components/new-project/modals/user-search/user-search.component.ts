e73a226efce9e23b7098ea0876a05763e1674e29
romantsinevich <braid98@yandex.ru>
2021-07-08 17:21:13 +0300

    Return role type and users for this role

--- src/app/modules/admin/components/new-project/modals/user-search/user-search.component.ts
+++ src/app/modules/admin/components/new-project/modals/user-search/user-search.component.ts
@@ -1,10 +1,15 @@
 import { Component, Inject, Input, OnDestroy, OnInit } from '@angular/core';
 import { Subscription } from 'rxjs';
 import { UserInfo } from '@data/api/user-service/models/user-info';
-import { MAT_DIALOG_DATA } from '@angular/material/dialog';
-import { Team, teamCards, TeamMember } from '../../team-cards';
-import { WorkFlowMode } from '../../../../../employee/employee-page.component';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+import { UserService } from '@app/services/user.service';
 import { UserSearchModalConfig } from '@app/services/modal.service';
+import { FormArray, FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { UserRoleType } from '@data/api/project-service/models/user-role-type';
+import { PositionInfo } from '@data/api/company-service/models/position-info';
+import { NetService } from '@app/services/net.service';
+import { Team, teamCards } from '../../team-cards';
+import { WorkFlowMode } from '../../../../../employee/employee-page.component';
 
 @Component({
 	selector: 'do-new-members-board',
@@ -14,24 +19,30 @@ import { UserSearchModalConfig } from '@app/services/modal.service';
 export class UserSearchComponent implements OnInit, OnDestroy {
 	@Input() mode: WorkFlowMode;
 	public members: UserInfo[];
-	public memberss: TeamMember[];
-	public membersAll: TeamMember[];
+	public membersAll: UserInfo[];
 	public visibleMembers: UserInfo[];
 	public filteredUsers: UserInfo[];
 	public checkedMembers: UserInfo[];
 	public selectedSpecialization;
 	public selectedLevel;
 	public searchName: string;
-	public specializations: string[] = ['Front-End Developer', 'Backend-End Developer', 'Product Manager', 'UI/UX Designer', 'QA Tester'];
-	public levels: string[] = ['Junior', 'Middle', 'Senior'];
+	public roles: UserRoleType[] = [UserRoleType.ProjectAdmin];
+	public positions: PositionInfo[];
 	public WorkFlowMode = WorkFlowMode;
+	public teamForm: FormGroup;
 
 	private getMembersSubscription: Subscription;
 
-	constructor(@Inject(MAT_DIALOG_DATA) public data: UserSearchModalConfig) {
+	constructor(
+		@Inject(MAT_DIALOG_DATA) public data: UserSearchModalConfig,
+		private _userService: UserService,
+		private _netService: NetService,
+		private _formBuilder: FormBuilder,
+		private _dialogRef: MatDialogRef<UserSearchComponent>
+	) {
 		this.checkedMembers = [];
 		this.searchName = null;
-		this.memberss = null;
+		this.members = null;
 		// TODO: Не показывать, пока не будет применён фильтр
 		switch (data.mode) {
 			case WorkFlowMode.VIEW: {
@@ -39,7 +50,7 @@ export class UserSearchComponent implements OnInit, OnDestroy {
 				break;
 			}
 			case WorkFlowMode.EDIT: {
-				this.memberss = data.team.members;
+				this.members = data.team.members;
 				this._initSearchMode();
 				break;
 			}
@@ -50,14 +61,48 @@ export class UserSearchComponent implements OnInit, OnDestroy {
 		// teamCards.forEach((team: Team) => {
 		//   this.membersAll.push(...team.members)
 		// });
+		this.teamForm = _formBuilder.group({
+			role: [null, [Validators.required]],
+			users: _formBuilder.array([]),
+		});
 	}
 
 	public ngOnInit(): void {
-		this.getMembers();
+		this._getMembers();
+		this._getPositions();
 	}
 
-	public getMembers(): void {
-		console.log('getMembers');
+	private _getMembers(): void {
+		this._userService.getUsers().subscribe(
+			(data) => {
+				this.membersAll = data;
+			},
+			(error) => console.log(error)
+		);
+	}
+
+	private _getPositions(): void {
+		this._netService.getPositionsList().subscribe(
+			(data) => {
+				this.positions = data;
+			},
+			(error) => console.log(error)
+		);
+	}
+
+	public onCheckboxChange(e): void {
+		const users: FormArray = this.teamForm.get('users') as FormArray;
+
+		if (e.checked) {
+			users.push(new FormControl(e.source.value));
+		} else {
+			users.controls.forEach((item: FormControl, i) => {
+				if (item.value === e.source.value) {
+					users.removeAt(i);
+					return;
+				}
+			});
+		}
 	}
 
 	public onSelect() {
@@ -80,18 +125,22 @@ export class UserSearchComponent implements OnInit, OnDestroy {
 		return this.visibleMembers;
 	}
 
+	public onSave(): void {
+		this._dialogRef.close(this.teamForm.value);
+	}
+
 	public onSearchClick(value: string): void {
 		this.searchName = value;
-		this.getMembers();
+		this._getMembers();
 	}
 
 	public ngOnDestroy(): void {
 		// this.getMembersSubscription.unsubscribe();
 	}
 
-	public assignLead(member: TeamMember): void {
-		const index: number = this.memberss.findIndex((teamMember: TeamMember) => teamMember === member);
-		this.memberss[index].lead = this.memberss[index].lead ? !this.memberss[index].lead : true;
+	public assignLead(member: UserInfo): void {
+		const index: number = this.members.findIndex((teamMember: UserInfo) => teamMember === member);
+		this.members[index].lead = this.members[index].lead ? !this.members[index].lead : true;
 	}
 
 	public onCheckMember($event, user): void {
@@ -108,10 +157,10 @@ export class UserSearchComponent implements OnInit, OnDestroy {
 		}
 	}
 
-	public isMember(user: TeamMember): boolean {
+	public isMember(user: UserInfo): boolean {
 		/* TODO: add id to compare with id*/
-		if (this.memberss && this.memberss.length) {
-			return this.memberss.some((member: TeamMember) => user.id === member.id);
+		if (this.members && this.members.length) {
+			return this.members.some((member: UserInfo) => user.id === member.id);
 		} else {
 			return false;
 		}
@@ -122,7 +171,7 @@ export class UserSearchComponent implements OnInit, OnDestroy {
 
 		this.membersAll = teamCards
 			.map((team: Team) => team.members)
-			.reduce((prev: TeamMember[], currentValue: TeamMember[]) => prev.concat(currentValue), []);
+			.reduce((prev: UserInfo[], currentValue: UserInfo[]) => prev.concat(currentValue), []);
 		console.log('initSearchMode');
 	}
 }
