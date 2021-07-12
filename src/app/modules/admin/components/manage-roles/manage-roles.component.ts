4c035d6eefdb95479e30e072e193fa02740f0f69
romantsinevich <braid98@yandex.ru>
2021-07-12 23:33:57 +0300

    Add pagination for role list

--- src/app/modules/admin/components/manage-roles/manage-roles.component.ts
+++ src/app/modules/admin/components/manage-roles/manage-roles.component.ts
@@ -1,26 +1,45 @@
 import { Component, OnInit } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 
-import { NewRoleComponent } from '../new-role/new-role.component';
+import { RoleInfo } from '@data/api/rights-service/models';
 import { RoleApiService } from '@data/api/rights-service/services';
-import { ROLES, Role } from './mock-roles';
-import { RoleInfo, RolesResponse } from '@data/api/rights-service/models';
+import { NewRoleComponent } from '../new-role/new-role.component';
+import { PageEvent } from '@angular/material/paginator';
 
 @Component({
-  selector: 'app-manage-roles',
-  templateUrl: './manage-roles.component.html',
-  styleUrls: ['./manage-roles.component.scss']
+	selector: 'app-manage-roles',
+	templateUrl: './manage-roles.component.html',
+	styleUrls: ['./manage-roles.component.scss'],
 })
 export class ManageRolesComponent implements OnInit {
+	public roles: RoleInfo[];
 
-  public roles: RoleInfo[];
-  constructor(private dialog: MatDialog, private roleApiService: RoleApiService) { }
+	public totalCount: number;
+	public pageSize: number;
+	public pageIndex: number;
 
-  public ngOnInit(): void {
-    this.roleApiService.findRoles({skipCount: 0, takeCount: 25}).subscribe(res => this.roles = res.roles)
-  }
+	constructor(private dialog: MatDialog, private roleApiService: RoleApiService) {
+		this.totalCount = 0;
+		this.pageSize = 10;
+		this.pageIndex = 0;
+	}
 
-  public onAddRoleClick(): void {
-    this.dialog.open(NewRoleComponent);
-  }
+	public ngOnInit(): void {
+		this.roleApiService.findRoles({ skipCount: this.pageIndex, takeCount: this.pageSize }).subscribe((res) => {
+			this.totalCount = res.totalCount;
+			this.roles = res.roles;
+		});
+	}
+
+	public onPageChange(event: PageEvent): void {
+		this.pageSize = event.pageSize;
+		this.pageIndex = event.pageIndex;
+		this.roleApiService.findRoles({ skipCount: this.pageIndex, takeCount: this.pageSize }).subscribe((data) => {
+			this.roles = data.roles;
+		});
+	}
+
+	public onAddRoleClick(): void {
+		this.dialog.open(NewRoleComponent);
+	}
 }
