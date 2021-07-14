af37ec95b8c2035b691f91c95f8fc7748a10d697
romantsinevich <braid98@yandex.ru>
2021-07-14 15:33:19 +0300

    Update profile-image component

--- src/app/shared/component/profile-image/profile-image.component.ts
+++ src/app/shared/component/profile-image/profile-image.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Input, ViewEncapsulation } from '@angular/core';
+import { ImageInfo } from '@data/api/user-service/models/image-info';
 
 @Component({
 	selector: 'do-profile-image',
@@ -7,9 +8,22 @@ import { Component, OnInit, Input, ViewEncapsulation } from '@angular/core';
 	encapsulation: ViewEncapsulation.None,
 })
 export class ProfileImageComponent implements OnInit {
-	@Input() src: string;
+	@Input() image: ImageInfo;
+	@Input() size: 'l' | 'sm';
+	public src: string;
+	public width: number;
 
-	constructor() {}
+	constructor() {
+		this.size = 'sm';
+		this.width = 48;
+	}
 
-	ngOnInit(): void {}
+	ngOnInit(): void {
+		if (this.image?.content && this.image?.extension) {
+			this.src = `data:image/${this.image.extension.slice(1)};base64,${this.image.content}`;
+		} else {
+			this.src = 'assets/images/IAFOR-Blank-Avatar-Image.jpg';
+		}
+		this.width = this.size === 'sm' ? 48 : 180;
+	}
 }
