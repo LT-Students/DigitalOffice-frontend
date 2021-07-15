e8a4e60c3e29f7fecc3aa18cf8c65c873f2efd3c
romantsinevich <braid98@yandex.ru>
2021-07-15 17:16:48 +0300

    Add check for token expiration

--- src/app/core/interceptors/auth.interceptor.ts
+++ src/app/core/interceptors/auth.interceptor.ts
@@ -2,17 +2,27 @@ import { Injectable } from '@angular/core';
 import { HttpInterceptor, HttpRequest, HttpHandler, HttpEvent, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
+import { Router } from '@angular/router';
 import { LocalStorageService } from '../services/local-storage.service';
 
 @Injectable({
 	providedIn: 'root',
 })
 export class AuthInterceptor implements HttpInterceptor {
-	constructor(private localStorageService: LocalStorageService) {}
+	constructor(private localStorageService: LocalStorageService, private _router: Router) {}
 
 	intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
 		const token = this.localStorageService.get('access_token');
 		let headers: HttpHeaders = req.headers.set('Access-Control-Allow-Origin', '*');
+
+		const jwtToken = JSON.parse(atob(token.split('.')[1]));
+		const expires = jwtToken.exp * 1000;
+
+		if (expires < Date.now()) {
+			this._router.navigate([ '/auth/login' ]);
+
+			return;
+		}
 
 		if (token) {
 			headers = headers.set('token', token);
