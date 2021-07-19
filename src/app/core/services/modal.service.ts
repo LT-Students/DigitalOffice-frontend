d54afafd09a8a9f7c58d4d19fec837f575e26ea4
Zhenya <epol1394@gmail.com>
2021-07-19 20:55:29 +0300

    typing the width of the popup window

--- src/app/core/services/modal.service.ts
+++ src/app/core/services/modal.service.ts
@@ -2,9 +2,9 @@ import { Injectable } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { ComponentType } from '@angular/cdk/overlay';
 import { MatDialogRef } from '@angular/material/dialog/dialog-ref';
-import { Team } from '../../modules/admin/components/new-project/team-cards';
-import { WorkFlowMode } from '../../modules/employee/employee-page.component';
 import { UserInfo } from '@data/api/user-service/models/user-info';
+import { WorkFlowMode } from '../../modules/employee/employee-page.component';
+import { Team } from '../../modules/admin/components/new-project/team-cards';
 
 export enum ModalType {
 	CREATE,
@@ -34,16 +34,27 @@ export interface UserSearchModalConfig {
 	mode: WorkFlowMode;
 }
 
+export const enum ModalWidth {
+	Download = '720px',
+	Remove = '440px',
+	NewCard = '672px',
+	Cancel = '550px',
+	ClosingCard = '700px',
+	Thumbnail = '647px',
+	Photo = '509px'
+}
+
+
 @Injectable({
 	providedIn: 'root',
 })
 export class ModalService {
 	constructor(private matDialog: MatDialog) {}
 
-	public openModal<C, T, R>(component: ComponentType<C>, modalContentConfig?: T, result?: R): MatDialogRef<C, R> {
+	public openModal<C, T, R>(component: ComponentType<C>, modalContentConfig?: T, result?: R, modalWidth?: ModalWidth): MatDialogRef<C, R> {
 		return this.matDialog.open<C, T, R>(component, {
 			data: modalContentConfig,
-			width: '720px',
+			width: modalWidth
 		});
 	}
 }
