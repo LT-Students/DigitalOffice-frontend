88fb0f39082a02c8a8f0ee91c2e9ab96f6b8590d
romantsinevich <braid98@yandex.ru>
2021-07-22 14:09:46 +0300

    Fix properties case at credentials response

--- src/app/data/api/user-service/models/operation-result-response-credentials-response.ts
+++ src/app/data/api/user-service/models/operation-result-response-credentials-response.ts
@@ -6,7 +6,7 @@ import { OperationResultStatusType } from './operation-result-status-type';
  * Response object for action operations.
  */
 export interface OperationResultResponseCredentialsResponse {
-  body?: { 'UserId'?: string, 'AccessToken'?: string, 'RefreshToken'?: string, 'accessTokenExpiresIn'?: number, 'refreshTokenExpiresIn'?: number };
+  body?: { 'userId'?: string, 'accessToken'?: string, 'refreshToken'?: string, 'accessTokenExpiresIn'?: number, 'refreshTokenExpiresIn'?: number };
   errors?: Array<string>;
   status?: OperationResultStatusType;
 }
