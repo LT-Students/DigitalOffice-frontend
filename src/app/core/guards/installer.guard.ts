9c3492c1814af2ce6b4c32f15f88d1eadb89df08
romantsinevich <braid98@yandex.ru>
2021-08-02 14:48:49 +0300

    Remove company from localStorage

--- src/app/core/guards/installer.guard.ts
+++ src/app/core/guards/installer.guard.ts
@@ -3,29 +3,20 @@ import { CanActivate, CanLoad, Route, UrlSegment, ActivatedRouteSnapshot, Router
 import { Observable } from 'rxjs';
 import { CompanyApiService } from '@data/api/company-service/services/company-api.service';
 import { map } from 'rxjs/operators';
-import { LocalStorageService } from '@app/services/local-storage.service';
 
 @Injectable({
 	providedIn: 'root',
 })
 export class InstallerGuard implements CanActivate, CanLoad {
-	constructor(private _companyApiService: CompanyApiService, private _localStorageService: LocalStorageService, private router: Router) {}
+	constructor(private _companyApiService: CompanyApiService, private router: Router) {}
 
 	canActivate(
 		route: ActivatedRouteSnapshot,
 		state: RouterStateSnapshot
 	): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-		const companyExists = this._localStorageService.get('company');
-
-		if (companyExists) {
-			return true;
-		}
-
 		return this._companyApiService.getCompany().pipe(
 			map((result) => {
 				if (result.body !== null) {
-					this._localStorageService.set('company', result.body);
-
 					return true;
 				}
 				this.router.navigate(['installer']);
