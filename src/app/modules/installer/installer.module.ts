8219a4191034425e8ae5735b57404d7c3098f247
Anastassia Varabei <nastenkaok1984@gmail.com>
2021-08-20 01:21:17 +0300

    fix some mistakes in installer.component.ts'

--- src/app/modules/installer/installer.module.ts
+++ src/app/modules/installer/installer.module.ts
@@ -3,9 +3,8 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MaterialModule } from '../../shared/material.module';
-import { WizardComponent } from './components/wizard/wizard.component';
-import { AppModule } from '../../app.module';
 import { SharedModule } from '../../shared/shared.module';
+import { WizardComponent } from './components/wizard/wizard.component';
 
 @NgModule({
 	declarations: [WizardComponent],
