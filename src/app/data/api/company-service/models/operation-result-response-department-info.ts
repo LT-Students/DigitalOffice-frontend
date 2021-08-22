273f8911b387c0331c46684d5799903286e35ad4
Zhenya <epol1394@gmail.com>
2021-08-22 23:47:12 +0300

    returned back changes in operation-result-response-departament-info

--- src/app/data/api/company-service/models/operation-result-response-department-info.ts
+++ src/app/data/api/company-service/models/operation-result-response-department-info.ts
@@ -5,11 +5,11 @@ import { DepartmentUserInfo } from './department-user-info';
 import { OperationResultStatusType } from './operation-result-status-type';
 import { ProjectInfo } from './project-info';
 export interface OperationResultResponseDepartmentInfo {
+
 	/**
 	 * Full departmant information.
 	 */
-
-	body?: { department?: DepartmentInfo; users?: Array<DepartmentUserInfo>; projects?: ProjectInfo };
+	body?: { 'department'?: DepartmentInfo, 'users'?: Array<DepartmentUserInfo>, 'projects'?: ProjectInfo };
 	errors?: Array<string>;
 	status?: OperationResultStatusType;
 }
