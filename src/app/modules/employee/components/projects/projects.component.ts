3eeaa0fc926b7c501e246a4edefecbc9c2527559
romantsinevich <braid98@yandex.ru>
2021-09-03 14:30:53 +0300

    Add projects section to employee page

--- src/app/modules/employee/components/projects/projects.component.ts
+++ src/app/modules/employee/components/projects/projects.component.ts
@@ -1,15 +1,9 @@
-import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { ProjectStatusType } from '@data/api/project-service/models/project-status-type';
 import { EmployeePageService } from '@app/services/employee-page.service';
 import { map } from 'rxjs/operators';
-import { ProjectInfo } from '@data/api/user-service/models/project-info';
-
-interface Group {
-	name: string;
-	projects: ProjectInfo[];
-	plural: { [p: string]: string };
-}
+import { Observable } from 'rxjs';
 
 @Component({
 	selector: 'do-employee-page-projects',
@@ -18,67 +12,36 @@ interface Group {
 	changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class ProjectsComponent implements OnInit {
-	public activeProjects: ProjectInfo[];
-	public closedProjects: ProjectInfo[];
-	public pluralForm: any;
-	public groups: Group[];
+	public projects$: Observable<any>;
 
 	constructor(private router: Router, private _employeeService: EmployeePageService) {
-		this.activeProjects = [];
-		this.closedProjects = [];
-		this.pluralForm = {
-			activeProjectsTitle: {
-				one: '# проект',
-				few: '# проекта',
-				other: '# проектов',
-			},
-			closedProjectsTitle: {
-				one: '# проекте',
-				other: '# проектах',
-			},
-		};
+		this.projects$ = this._employeeService.selectedUser$.pipe(
+			map((user) => user?.projects),
+			map((projects) => [
+				{
+					name: 'В работе',
+					projects: projects?.filter((project) => project.status === ProjectStatusType.Active) ?? [],
+					plural: {
+						one: '# проект',
+						few: '# проекта',
+						other: '# проектов',
+					},
+				},
+				{
+					name: 'Участвовал в',
+					projects: projects?.filter((project) => project.status !== ProjectStatusType.Active) ?? [],
+					plural: {
+						one: '# проекте',
+						other: '# проектах',
+					},
+				},
+			])
+		);
 	}
 
-	public ngOnInit(): void {
-		this._employeeService.selectedUser$.pipe(
-			map((user) => user?.projects)
-		).subscribe({
-			next: (projects) => {
-				this._filterProject(projects);
-			}
-		})
-		this.groups = [
-			{
-				name: 'В работе',
-				projects: this.activeProjects,
-				plural: this.pluralForm.activeProjectsTitle,
-			},
-			{
-				name: 'Участвовал в',
-				projects: this.closedProjects,
-				plural: this.pluralForm.closedProjectsTitle,
-			},
-		];
-	}
-
-	public expandProjects(group): void {
-		group.expanded = !group.expanded;
-	}
+	public ngOnInit(): void {}
 
 	public onMoreClicked(projectId: string | undefined) {
 		this.router.navigate(['/project', projectId]);
-	}
-
-	private _filterProject(projects: ProjectInfo[] | null | undefined) {
-		if (projects != null) {
-			this.activeProjects = projects.filter((project: ProjectInfo) => {
-				const projectStatus = project.status as ProjectStatusType;
-				return projectStatus === ProjectStatusType.Active;
-			});
-			this.closedProjects = projects.filter((project: ProjectInfo) => {
-				const projectStatus = project.status as ProjectStatusType;
-				return projectStatus === ProjectStatusType.Closed;
-			});
-		}
 	}
 }
