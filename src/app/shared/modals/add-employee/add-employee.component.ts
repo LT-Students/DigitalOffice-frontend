67095637a74845a8df5724b9c0a99f524efeb55d
Randattt <rvlad2109@mail.ru>
2021-10-12 21:32:54 +0300

    refactor(Show list of users from server): resolve all conversations #3

--- src/app/shared/modals/add-employee/add-employee.component.ts
+++ src/app/shared/modals/add-employee/add-employee.component.ts
@@ -12,50 +12,47 @@ import { UserInfo } from '@data/api/user-service/models/user-info';
 export class AddEmployeeComponent implements OnInit {
 	public positions: string[];
 	public employees: UserInfo[];
-	public pageSize: number;
-	public pageIndex: number;
+	private _usersListSize: number;
+	private _skipUsers: number;
 
 	constructor(
 		private _userService: UserService,
 		private _cdr: ChangeDetectorRef,
-		public dialogRef: MatDialogRef<AddEmployeeComponent>,
-		@Inject(MAT_DIALOG_DATA) public data: { idToHide: string[] }
+		private _dialogRef: MatDialogRef<AddEmployeeComponent>,
+		@Inject(MAT_DIALOG_DATA) private _data: { idToHide: string[] }
 	) {
 		this.positions = ['front', 'back', 'manager', 'lead'];
 		this.employees = [];
-		this.pageSize = 5;
-		this.pageIndex = 0;
+		this._usersListSize = 6;
+		this._skipUsers = 0;
 	}
 	public ngOnInit(): void {
-		this._getPageUsers();
+		this.getPageUsers();
 	}
 
-	onNoClick(): void {
-		this.dialogRef.close();
+	public onClose(): void {
+		this._dialogRef.close();
 	}
 
-	private _getPageUsers(): void {
+	public getPageUsers(): void {
 		this._userService
 			.findUsers({
-				skipCount: this.pageIndex,
-				takeCount: this.pageSize,
+				skipCount: this._skipUsers,
+				takeCount: this._usersListSize,
 				includedepartment: true,
 				includeposition: true,
 			})
 			.subscribe((data) => {
-				// eslint-disable-next-line @typescript-eslint/no-unused-expressions
-				data?.body?.forEach((el) => {
-					this.employees.push(el);
-				}) ?? [];
-				this.employees = this.employees.filter((e) => this.data.idToHide.indexOf(e.id as string) === -1);
-				console.log(this.employees);
-				this.pageIndex += this.employees.length + this.data.idToHide.length;
-				this.pageSize += this.employees.length + this.data.idToHide.length;
-				this._cdr.markForCheck();
+				if (data.body !== undefined) {
+					data.body = data.body.filter((e) => this._data.idToHide.indexOf(e.id as string) === -1);
+					this.employees.push(...data.body);
+					this._skipUsers += data.body.length;
+					this._cdr.markForCheck();
+				}
 			});
 	}
 
 	public onScroll() {
-		this._getPageUsers();
+		this.getPageUsers();
 	}
 }
