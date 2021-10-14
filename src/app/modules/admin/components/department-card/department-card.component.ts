7aee1b01d911e1277c500c3c0ebd3937a3066746
Randattt <rvlad2109@mail.ru>
2021-10-14 23:29:35 +0300

    refactor(New template): small changes for better view and code

--- src/app/modules/admin/components/department-card/department-card.component.ts
+++ src/app/modules/admin/components/department-card/department-card.component.ts
@@ -175,18 +175,17 @@ export class DepartmentCardComponent implements OnInit {
 		});
 	}
 
-	isAllSelected() {
+	public isAllSelected(): boolean {
 		const numSelected = this.selection.selected.length;
 		const numRows = this.dataSource.data.length;
 		return numSelected === numRows;
 	}
 
-	masterToggle() {
+	public masterToggle(): void {
 		if (this.isAllSelected()) {
 			this.selection.clear();
 			return;
 		}
-
 		this.selection.select(...this.dataSource.data);
 	}
 }
