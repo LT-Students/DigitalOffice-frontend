c574494729c39e1fac5d792ed4ba6120cac7491b
Dimokku <trporange@gmail.com>
2021-10-15 17:20:04 +0300

    fixed [3/5 files]

--- src/app/shared/component/stepper/stepper.component.ts
+++ src/app/shared/component/stepper/stepper.component.ts
@@ -1,22 +1,34 @@
-import { Component, Input, ChangeDetectionStrategy, HostBinding, OnDestroy, Optional, Self, OnChanges, SimpleChanges } from '@angular/core';
+import {
+	Component,
+	Input,
+	ChangeDetectionStrategy,
+	HostBinding,
+	OnDestroy,
+	Optional,
+	Self,
+	OnChanges,
+	SimpleChanges,
+} from '@angular/core';
 import { ControlValueAccessor, FormControl, NgControl } from '@angular/forms';
 import { MatFormFieldControl } from '@angular/material/form-field';
 import { DoValidators } from '@app/validators/do-validators';
 import { Subject } from 'rxjs';
+import { coerceBooleanProperty } from '@angular/cdk/coercion';
 
 @Component({
 	selector: 'do-stepper',
 	templateUrl: './stepper.component.html',
 	styleUrls: ['./stepper.component.scss'],
 	changeDetection: ChangeDetectionStrategy.OnPush,
-	providers: [{
-		provide: MatFormFieldControl,
-		useExisting: StepperComponent
-	}]
+	providers: [
+		{
+			provide: MatFormFieldControl,
+			useExisting: StepperComponent,
+		},
+	],
 })
-export class StepperComponent implements OnDestroy, OnChanges, MatFormFieldControl<number>, ControlValueAccessor {
-	@Input()
-	public label: string;
+export class StepperComponent implements OnDestroy, MatFormFieldControl<number>, ControlValueAccessor {
+	static uniqueId = 0;
 
 	@Input()
 	public step: number;
@@ -38,10 +50,11 @@ export class StepperComponent implements OnDestroy, OnChanges, MatFormFieldContr
 		}
 
 		this.stateChanges.next();
-	};
+	}
+
 	public get value(): number {
 		return this.stepperControl.value;
-	};
+	}
 
 	public stateChanges: Subject<void>;
 
@@ -52,10 +65,11 @@ export class StepperComponent implements OnDestroy, OnChanges, MatFormFieldContr
 	public set placeholder(value: string) {
 		this._placeholder = value;
 		this.stateChanges.next();
-	};
+	}
+
 	public get placeholder(): string {
 		return this._placeholder ?? '';
-	};
+	}
 
 	public focused: boolean;
 
@@ -64,13 +78,37 @@ export class StepperComponent implements OnDestroy, OnChanges, MatFormFieldContr
 	@HostBinding()
 	public get shouldLabelFloat(): boolean {
 		return true;
-	};
+	}
 
 	@Input()
-	public required: boolean;
+	public set required(value: boolean | string) {
+		this._required = coerceBooleanProperty(value);
+		this.stateChanges.next();
+	}
+
+	public get required(): boolean {
+		return this._required;
+	}
+
+	private _required: boolean;
 
 	@Input()
-	public disabled: boolean;
+	public set disabled(value: boolean) {
+		console.log('THIS IS VALUE: ', value);
+		this._disabled = coerceBooleanProperty(value);
+		if (this.disabled) {
+			this.stepperControl.disable();
+		} else {
+			this.stepperControl.enable();
+		}
+		this.stateChanges.next();
+	}
+
+	public get disabled(): boolean {
+		return this._disabled;
+	}
+
+	private _disabled: boolean;
 
 	public errorState: boolean;
 
@@ -79,30 +117,25 @@ export class StepperComponent implements OnDestroy, OnChanges, MatFormFieldContr
 	@HostBinding('attr.aria-describedby')
 	public userAriaDescribedBy?: string | undefined;
 
-	static uniqueId: number = 0;
-
 	private _placeholder?: string;
 
 	public stepperControl: FormControl;
 
-	constructor(
-		@Optional() @Self() public ngControl: NgControl | null
-	) {
+	constructor(@Optional() @Self() public ngControl: NgControl | null) {
+		this._required = false;
+		this._disabled = false;
 		if (this.ngControl != null) {
 			this.ngControl.valueAccessor = this;
 		}
-		this.label = "";
 		this.focused = true;
 		this.stateChanges = new Subject<void>();
-		this.stepperControl = new FormControl(0, [DoValidators.number])
-		this.placeholder = "";
+		this.stepperControl = new FormControl(0, [DoValidators.number]);
+		this.placeholder = '';
 		this.step = 1;
 		this.min = Number.MIN_SAFE_INTEGER;
 		this.max = Number.MAX_SAFE_INTEGER;
-		this.id = `user-stepper-field-id-${StepperComponent.uniqueId++}`;
-		this.required = false;
+		this.id = `custom-stepper-field-id-${StepperComponent.uniqueId++}`;
 		this.empty = false;
-		this.disabled = false;
 		this.errorState = false;
 		this.userAriaDescribedBy = '';
 		this.ngControl = null;
@@ -112,19 +145,20 @@ export class StepperComponent implements OnDestroy, OnChanges, MatFormFieldContr
 		this.value = Number(value);
 	}
 
-	public onChange(value: number): void { }
+	public onChange(value: number): void {}
+
 	public registerOnChange(fn: any): void {
 		this.onChange = fn;
 	}
 
-	public onTouch(): void { }
+	public onTouch(): void {}
+
 	public registerOnTouched(fn: any): void {
 		this.onTouch = fn;
 	}
 
 	public setDisabledState(isDisabled: boolean) {
 		this.disabled = isDisabled;
-		isDisabled ? this.stepperControl.disable() : this.stepperControl.enable();
 		this.stateChanges.next();
 	}
 
@@ -132,9 +166,7 @@ export class StepperComponent implements OnDestroy, OnChanges, MatFormFieldContr
 		this.userAriaDescribedBy = ids.join(' ');
 	}
 
-	public onContainerClick(event: MouseEvent): void {
-
-	}
+	public onContainerClick(event: MouseEvent): void {}
 
 	public onChangeClick(step: number) {
 		this.value += step;
@@ -142,11 +174,11 @@ export class StepperComponent implements OnDestroy, OnChanges, MatFormFieldContr
 		this.onChange(this.value);
 	}
 
-	public ngOnChanges(changes: SimpleChanges): void {
-		if (changes.disabled?.currentValue !== changes.disabled?.previousValue) {
-			this.setDisabledState(changes.disabled.currentValue);
-		}
-	}
+	// public ngOnChanges(changes: SimpleChanges): void {
+	// 	if (changes.disabled?.currentValue !== changes.disabled?.previousValue) {
+	// 		this.setDisabledState(changes.disabled.currentValue);
+	// 	}
+	// }
 
 	ngOnDestroy() {
 		this.stateChanges.complete();
