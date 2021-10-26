e2d38e8608a12d5c900e5fb15f6c1db8151ee8eb
Randattt <rvlad2109@mail.ru>
2021-10-26 20:55:10 +0300

    feat(Input number mask): create mask for tel number input, small changes to template

--- src/app/modules/employee/components/main-info/communications/edit-contact/edit-contact.component.ts
+++ src/app/modules/employee/components/main-info/communications/edit-contact/edit-contact.component.ts
@@ -49,6 +49,68 @@ export class EditContactComponent {
 		this._matDialogRef.close(result);
 	}
 
+	private _getInputNumbersValue(input: any) {
+		return input.value.replace(/\D/g, '');
+	}
+
+	public onPhoneInput(e: any) {
+		let input = e.target;
+		let inputNumbersValue = this._getInputNumbersValue(input);
+		let formattedInputValue = '';
+		let selectionStart = input.selectionStart;
+
+		if (!inputNumbersValue) {
+			return (input.value = '');
+		}
+
+		if (input.value.length !== selectionStart) {
+			if (e.data && /\D/g.test(e.data)) {
+				input.value = formattedInputValue;
+			}
+			return;
+		}
+
+		if (['7', '8', '9'].includes(inputNumbersValue[0])) {
+			if (inputNumbersValue[0] === '9') inputNumbersValue = '7' + inputNumbersValue;
+			let firstSymbols = '+7';
+			formattedInputValue = input.value = firstSymbols + ' ';
+			if (inputNumbersValue.length > 1) {
+				formattedInputValue += '(' + inputNumbersValue.substring(1, 4);
+			}
+			if (inputNumbersValue.length >= 5) {
+				formattedInputValue += ') ' + inputNumbersValue.substring(4, 7);
+			}
+			if (inputNumbersValue.length >= 8) {
+				formattedInputValue += '-' + inputNumbersValue.substring(7, 9);
+			}
+			if (inputNumbersValue.length >= 10) {
+				formattedInputValue += '-' + inputNumbersValue.substring(9, 11);
+			}
+		} else {
+			formattedInputValue = '+' + inputNumbersValue.substring(0, 16);
+		}
+		input.value = formattedInputValue;
+	}
+
+	public OnPhoneKeyDown(e: any) {
+		if (this._getInputNumbersValue(e.target).length === 1) {
+			e.target.value = '';
+		}
+	}
+
+	public onPhonePaste(e: any) {
+		let input = e.target,
+			inputNumbersValue = this._getInputNumbersValue(input);
+		let pasted = e.clipboardData;
+		if (pasted) {
+			let pastedText = pasted.getData('Text');
+			if (/\D/g.test(pastedText)) {
+				input.value = inputNumbersValue;
+				return;
+			}
+		}
+	}
+
 	public onSubmit(): void {
 		this.loading.next(true);
 
