b63596118fe8a1a829ac3bbc1eb53c490dc9bd25
Dimokku <trporange@gmail.com>
2021-11-10 23:53:34 +0300

    updated project service

---
+++ src/app/data/api/project-service/models/file-content.ts
@@ -0,0 +1,18 @@
+/* tslint:disable */
+/* eslint-disable */
+export interface FileContent {
+	/**
+	 * File content.
+	 */
+	content: string;
+
+	/**
+	 * Extension of the file.
+	 */
+	extension: string;
+
+	/**
+	 * Name of the file.
+	 */
+	name?: null | string;
+}
