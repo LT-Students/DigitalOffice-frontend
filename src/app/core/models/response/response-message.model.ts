d6cd18e550dd53a300147e95aba5ffe3b53cd447
Randattt <rvlad2109@mail.ru>
2021-11-16 17:51:35 +0300

    refactor(Response message model): add processing of success status "PartialSuccess" + small fixes

--- src/app/core/models/response/response-message.model.ts
+++ src/app/core/models/response/response-message.model.ts
@@ -15,7 +15,7 @@ export interface IErrorMessageTypes {
 export class ResponseMessageModel {
 	constructor(private _snackBar: MatSnackBar) {}
 
-	private _errorMessageTypes: IErrorMessageTypes[] = [
+	private _responseMessageTypes: IErrorMessageTypes[] = [
 		{ triggered: MessageTriggeredFrom.Project, feminine: false },
 		{ triggered: MessageTriggeredFrom.Department, feminine: false },
 		{ triggered: MessageTriggeredFrom.Office, feminine: false },
@@ -27,20 +27,28 @@ export class ResponseMessageModel {
 		{ triggered: MessageTriggeredFrom.News, feminine: true },
 	];
 
-	public getSuccessMessage(triggeredFrom: MessageTriggeredFrom, method: MessageMethod): string {
-		const result = this._errorMessageTypes.find((item) => item.triggered === triggeredFrom);
-		return `${triggeredFrom} успешно ${method}${result?.feminine === true ? 'а' : ''}`;
+	public getSuccessMessage(triggeredFrom: MessageTriggeredFrom, method: MessageMethod, success: any): string {
+		const result = this._responseMessageTypes.find((item) => item.triggered === triggeredFrom);
+		let successMessage = `${triggeredFrom} успешно ${method}${result?.feminine === true ? 'а' : ''}`;
+		if (success.status === 'PartialSuccess') {
+			successMessage = 'Выполнено частично';
+		}
+		return successMessage;
 	}
 
-	public showSuccessMessage(triggeredFrom: MessageTriggeredFrom, method: MessageMethod): void {
-		const result = this._errorMessageTypes.find((item) => item.triggered === triggeredFrom);
-		this._snackBar.open(`${triggeredFrom} успешно ${method}${result?.feminine === true ? 'а' : ''}`, 'done', {
+	public showSuccessMessage(triggeredFrom: MessageTriggeredFrom, method: MessageMethod, success: any): void {
+		const result = this._responseMessageTypes.find((item) => item.triggered === triggeredFrom);
+		let successMessage = `${triggeredFrom} успешно ${method}${result?.feminine === true ? 'а' : ''}`;
+		if (success.status === 'PartialSuccess') {
+			successMessage = 'Выполнено частично';
+		}
+		this._snackBar.open(successMessage, '×', {
 			duration: 3000,
 		});
 	}
 
 	public getErrorMessage(err: any): string {
-		let errorMessage: string = err.error.errors?.[0] ?? 'Что-то пошло не так :(';
+		let errorMessage: string = err.error.errors?.join(' ') ?? 'Что-то пошло не так :(';
 		if (err.status === 403) {
 			errorMessage = 'Недостаточно прав доступа';
 		}
@@ -51,13 +59,13 @@ export class ResponseMessageModel {
 	}
 
 	public showErrorMessage(err: any): void {
-		let errorMessage: string = err.error.errors?.[0] ?? 'Что-то пошло не так :(';
+		let errorMessage: string = err.error.errors?.join(' ') ?? 'Что-то пошло не так :(';
 		if (err.status === 403) {
 			errorMessage = 'Недостаточно прав доступа';
 		} else if (err.status === 404) {
 			errorMessage = 'Операция отклонена';
 		}
-		this._snackBar.open(errorMessage, '', {
+		this._snackBar.open(errorMessage, '×', {
 			duration: 3000,
 		});
 	}
@@ -69,8 +77,8 @@ export class ResponseMessageModel {
 					this.showErrorMessage(err);
 					return throwError(err);
 				}),
-				tap(() => {
-					this.showSuccessMessage(triggeredFrom, method);
+				tap((result) => {
+					this.showSuccessMessage(triggeredFrom, method, result);
 				})
 			);
 		};
