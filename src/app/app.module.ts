7b6fc6f70ddc472d4443bdac263f528d14549ee5
Zhenya <epol1394@gmail.com>
2021-11-23 21:01:11 +0300

    fixed condition

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -28,7 +28,7 @@ import { NewsModule } from './modules/news/news.module';
 registerLocaleData(localeRu);
 
 function initializeCompanyAndUser(appInitService: AppInitService) {
-	return (): Promise<any> | undefined => {
+	return (): Promise<any> => {
 		return appInitService.getCompanyAndUser();
 	};
 }
