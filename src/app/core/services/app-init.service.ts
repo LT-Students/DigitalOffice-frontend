8abc24d50dbe935961e2e10b5f632239eed5bb6c
Zhenya <epol1394@gmail.com>
2021-11-23 21:01:11 +0300

    fixed condition

--- src/app/core/services/app-init.service.ts
+++ src/app/core/services/app-init.service.ts
@@ -5,6 +5,8 @@ import { AuthService } from '@app/services/auth/auth.service';
 import { CompanyService } from '@app/services/company/company.service';
 import { CurrentUserService } from '@app/services/current-user.service';
 import { CurrentCompanyService } from '@app/services/current-company.service';
+import { EMPTY, iif, Observable } from 'rxjs';
+import { User } from '@app/models/user/user.model';
 
 @Injectable({
 	providedIn: 'root',
@@ -18,28 +20,27 @@ export class AppInitService {
 		private _authService: AuthService
 	) {}
 
-	public getCompanyAndUser(): Promise<any> | undefined {
+	public getCompanyAndUser(): Promise<void> {
 		const token: string | null = this._localStorage.get('access_token');
 
 		const userId: string | undefined = token ? (JSON.parse(atob(token.split('.')[1])).UserId as string) : undefined;
 		return new Promise((resolve) => {
-			if (token === null) {
-				this._companyService
-					.getCompany()
-					.pipe(tap((company) => this._currentCompanyService.setCompany(company)))
-					.subscribe()
-					.add(resolve);
-			} else {
-				this._companyService
-					.getCompany()
-					.pipe(
-						tap((company) => this._currentCompanyService.setCompany(company)),
-						switchMap(() => this._currentUserService.getUserOnLogin(userId)),
-						tap((user) => this._currentUserService.setUser(user))
+			this._companyService
+				.getCompany()
+				.pipe(
+					tap((company) => this._currentCompanyService.setCompany(company)),
+					switchMap(() =>
+						iif(
+							() => token !== null,
+							this._currentUserService
+								.getUserOnLogin(userId)
+								.pipe(tap((user) => this._currentUserService.setUser(user))),
+							EMPTY
+						)
 					)
-					.subscribe()
-					.add(resolve);
-			}
+				)
+				.subscribe()
+				.add(resolve);
 		});
 	}
 }
