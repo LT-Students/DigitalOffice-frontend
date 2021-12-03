99dc6d2a0506695787ef6ef176625a96c2b6c099
Dimokku <trporange@gmail.com>
2021-12-04 00:12:30 +0300

    improvement(fix installer guard): fixed return value from canActivate

--- src/app/core/guards/installer.guard.ts
+++ src/app/core/guards/installer.guard.ts
@@ -9,9 +9,11 @@ import {
 	UrlTree,
 	Router,
 } from '@angular/router';
-import { Observable } from 'rxjs';
+import { Observable, of } from 'rxjs';
 import { CurrentCompanyService } from '@app/services/current-company.service';
-import { map, tap } from 'rxjs/operators';
+import { map, switchMap, tap } from 'rxjs/operators';
+import { fromPromise } from 'rxjs/internal-compatibility';
+import { Company } from '@app/models/company';
 import { RouteType } from '../../app-routing.module';
 
 @Injectable({
@@ -25,16 +27,28 @@ export class InstallerGuard implements CanActivate, CanLoad {
 		state: RouterStateSnapshot
 	): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
 		return this._currentCompanyService.company$.pipe(
-			map((company) => !!company),
-			tap((companyExists) => {
+			// map((company) => !!company),
+			// tap((companyExists) => {
+			// 	if (!companyExists) {
+			// 		if (route.routeConfig?.path !== RouteType.INSTALLER) {
+			// 			this._router.navigate([RouteType.INSTALLER]);
+			// 		}
+			// 	} else {
+			// 		if (route.routeConfig?.path !== '') {
+			// 			this._router.navigate(['']);
+			// 		}
+			// 	}
+			// })
+			map((company: Company) => !!company.id),
+			switchMap((companyExists: boolean) => {
 				if (!companyExists) {
 					if (route.routeConfig?.path !== RouteType.INSTALLER) {
-						this._router.navigate([RouteType.INSTALLER]);
-					}
+						return fromPromise(this._router.navigate([RouteType.INSTALLER]));
+					} else return of(true);
 				} else {
 					if (route.routeConfig?.path !== '') {
-						this._router.navigate(['']);
-					}
+						return fromPromise(this._router.navigate(['']));
+					} else return of(true);
 				}
 			})
 		);
