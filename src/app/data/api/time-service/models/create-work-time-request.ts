02c973c659735452ca39d71ff4eb23a1b7b11a01
Dimokku <trporange@gmail.com>
2021-12-12 03:24:51 +0300

    improvement(update services): update TimeService to 1.1.7.3

---
+++ src/app/data/api/time-service/models/create-work-time-request.ts
@@ -0,0 +1,8 @@
+/* tslint:disable */
+/* eslint-disable */
+export interface CreateWorkTimeRequest {
+	description?: string;
+	hours?: number;
+	month: number;
+	year: number;
+}
--- src/app/data/api/time-service/models/work-time-info.ts
+++ src/app/data/api/time-service/models/work-time-info.ts
@@ -3,23 +3,27 @@
 import { ProjectInfo } from './project-info';
 import { WorkTimeJobInfoInfo } from './work-time-job-info-info';
 export interface WorkTimeInfo {
-  day?: null | number;
+	day?: null | number;
 
-  /**
-   * Description for alloted time.
-   */
-  description?: string;
+	/**
+	 * Description for alloted time.
+	 */
+	description?: string;
 
-  /**
-   * The work time Id.
-   */
-  id?: string;
-  jobs?: null | Array<WorkTimeJobInfoInfo>;
-  managerHours?: null | number;
-  modifiedAtUtc?: null | string;
-  month?: number;
-  project?: ProjectInfo;
-  userHours?: null | number;
-  year?: number;
+	/**
+	 * The work time Id.
+	 */
+	id?: string;
+	jobs?: null | Array<WorkTimeJobInfoInfo>;
+
+	/**
+	 * Description for alloted time.
+	 */
+	managerDescription?: string;
+	managerHours?: null | number;
+	modifiedAtUtc?: null | string;
+	month?: number;
+	project?: ProjectInfo;
+	userHours?: null | number;
+	year?: number;
 }
-
