fef2711050b56a2c14791d53f42d79f20f9a5064
Romanov Vladislav <rvlad0921@mail.ru>
2022-01-14 16:36:32 +0300

    feature(Scroll after route to 0): now scroll returns to the zero point when we're switching to another module

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,33 @@
 //@ts-nocheck
-import { Component, ChangeDetectionStrategy } from '@angular/core';
+import { Component, ChangeDetectionStrategy, OnInit, OnDestroy } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { filter } from 'rxjs/operators';
+import { NavigationEnd, Router } from '@angular/router';
 
 @Component({
-  selector: 'do-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss'],
-changeDetection: ChangeDetectionStrategy.OnPush,
+	selector: 'do-root',
+	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.scss'],
+	changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class AppComponent {
-  public user = {
-    firstName: 'Ия',
-  };
+export class AppComponent implements OnInit, OnDestroy {
+	private _subscription: Subscription;
+	public user = {
+		firstName: 'Ия',
+	};
+
+	constructor(private _router: Router) {}
+
+	public ngOnInit(): void {
+		this._subscription = this._router.events
+			.pipe(filter((event) => event instanceof NavigationEnd))
+			.subscribe(() => {
+				const element = document.querySelector('mat-sidenav-content') || window;
+				element.scrollTo(0, 0);
+			});
+	}
+
+	public ngOnDestroy(): void {
+		this._subscription.unsubscribe();
+	}
 }
