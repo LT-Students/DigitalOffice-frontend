7f917e2a9706d7b7f14e3d7f42529f61a9e13f9b
romantsinevich <braid98@yandex.ru>
2022-04-20 17:31:03 +0300

    Override outline material styles

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -9,16 +9,12 @@ import { NewEmployeeComponent } from '@shared/modals/new-employee/new-employee.c
 import { AddEditDepartmentComponent } from '@shared/modals/add-edit-department/add-edit-department.component';
 import { UserStatusPipe } from '@shared/pipes/user-status.pipe';
 import { PlaceholderPipe } from '@shared/pipes/placeholder.pipe';
-import { User } from '@app/models/user/user.model';
 import { UserRecoveryComponent } from '@shared/modals/user-recovery/user-recovery.component';
 import { MaterialModule } from './modules/material/material.module';
 
 import { ProfileImageComponent } from './component/profile-image/profile-image.component';
 import { ContentContainerComponent } from './component/content-container/content-container.component';
-import { SearchComponent } from './component/search/search.component';
 import { HeaderComponent } from './component/header/header.component';
-import { InputComponent } from './component/input/input.component';
-import { DatepickerComponent } from './component/datepicker/datepicker.component';
 import { StepperComponent } from './component/stepper/stepper.component';
 import { PasswordComponent } from './component/password/password.component';
 import { CommentComponent } from './component/comment/comment.component';
@@ -42,15 +38,13 @@ import { PasswordHintComponent } from './component/password-hint/password-hint.c
 import { TruncateTooltipDirective } from './directives/truncate-tooltip.directive';
 import { ProjectTypePipe } from './pipes/project-type.pipe';
 import { LeaveLabelPipe } from './pipes/leave-label.pipe';
+import { FormRowComponent } from './component/form-row/form-row.component';
 
 @NgModule({
 	declarations: [
 		ProfileImageComponent,
 		ContentContainerComponent,
-		SearchComponent,
 		HeaderComponent,
-		InputComponent,
-		DatepickerComponent,
 		StepperComponent,
 		TextDirective,
 		DndDirective,
@@ -78,6 +72,7 @@ import { LeaveLabelPipe } from './pipes/leave-label.pipe';
 		UserStatusPipe,
 		PlaceholderPipe,
 		UserRecoveryComponent,
+		FormRowComponent,
 	],
 	imports: [
 		CommonModule,
@@ -98,14 +93,11 @@ import { LeaveLabelPipe } from './pipes/leave-label.pipe';
 		MdePopoverModule,
 		ReactiveFormsModule,
 		FormsModule,
-		InputComponent,
 		ProfileImageComponent,
-		SearchComponent,
 		HeaderComponent,
 		TextDirective,
 		DndDirective,
 		ButtonLoadingDirective,
-		DatepickerComponent,
 		StepperComponent,
 		SafeHtmlPipe,
 		PasswordComponent,
