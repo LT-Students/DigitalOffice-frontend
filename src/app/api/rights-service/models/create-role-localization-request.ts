6fe7b60cf8f0bae7347dbbb841e48126540ced85
romantsinevich <braid98@yandex.ru>
2022-05-06 14:29:00 +0500

    Update Rights Service

--- src/app/api/rights-service/models/create-role-localization-request.ts
+++ src/app/api/rights-service/models/create-role-localization-request.ts
@@ -1,12 +1,13 @@
 /* tslint:disable */
 /* eslint-disable */
 export interface CreateRoleLocalizationRequest {
-	description?: string;
-	locale: string;
-	name: string;
+  description?: string;
+  locale: string;
+  name: string;
 
-	/**
-	 * Role id.
-	 */
-	roleId?: string;
+  /**
+   * Role id.
+   */
+  roleId?: string;
 }
+
