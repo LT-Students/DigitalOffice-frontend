da4e8d4f1cc3d893bff26becbdddb1496d64bb4d
kohuro <sharf.toto.fly@gmail.com>
2022-05-13 11:13:05 +0300

    Created a guard

--- src/app/core/services/permission.service.ts
+++ src/app/core/services/permission.service.ts
@@ -2,18 +2,25 @@ import { Injectable } from '@angular/core';
 import { BehaviorSubject, Observable, ObservedValuesFromArray, Observer, Subject, Subscriber } from 'rxjs';
 import { map, take } from 'rxjs/operators';
 import { UserRights } from '@app/types/user-rights.enum';
+import { User } from '@app/models/user/user.model';
 import { CurrentUserService } from './current-user.service';
 
 @Injectable({
 	providedIn: 'root',
 })
 export class PermissionService {
-	private assignesRights$: Observable<UserRights[]>;
+	private assignedRights$: Observable<UserRights[]>;
+	private isAdmin$: Observable<boolean>;
 
 	constructor(private currentUserService: CurrentUserService) {
-		this.assignesRights$ = this.currentUserService.user$.pipe(
-			map((user) => user?.role?.rightsIds ?? []),
-			take(1)
+		this.assignedRights$ = this.currentUserService.user$.pipe(
+			take(1),
+			map((user) => user?.role?.rightsIds ?? [])
+		);
+
+		this.isAdmin$ = this.currentUserService.user$.pipe(
+			take(1),
+			map((user) => user.isAdmin)
 		);
 	}
 
@@ -21,19 +28,14 @@ export class PermissionService {
 		let isAdmin = false;
 		let hasPermission = false;
 
-		this.currentUserService.user$
-			.pipe(
-				map((user) => user.isAdmin),
-				take(1)
-			)
-			.subscribe({
-				next: (admin) => (isAdmin = admin),
-			});
+		this.isAdmin$.subscribe({
+			next: (admin) => (isAdmin = admin),
+		});
 
 		if (isAdmin) {
 			return true;
 		} else {
-			this.assignesRights$.subscribe({
+			this.assignedRights$.subscribe({
 				next: (rights: UserRights[]) => (hasPermission = rights.includes(right)),
 			});
 
