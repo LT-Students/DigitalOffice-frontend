abdcf7f03f7f81d5c9bd6da9f23cd393b4c83c73
romantsinevich <braid98@yandex.ru>
2022-06-30 12:49:30 +0300

    Update Rights Service

--- src/app/api/rights-service/models/operation-result-response.ts
+++ src/app/api/rights-service/models/operation-result-response.ts
@@ -1,14 +1,10 @@
 /* tslint:disable */
 /* eslint-disable */
-import { OperationResultStatusType } from './operation-result-status-type';
 
 /**
  * Response object for action operations.
  */
 export interface OperationResultResponse {
-  body?: {
-};
-  errors?: Array<string>;
-  status?: OperationResultStatusType;
+	body?: {};
+	errors: Array<string>;
 }
-
--- src/app/api/rights-service/models/user-info.ts
+++ src/app/api/rights-service/models/user-info.ts
@@ -1,9 +1,8 @@
 /* tslint:disable */
 /* eslint-disable */
 export interface UserInfo {
-  firstName?: string;
-  id?: string;
-  lastName?: string;
-  middleName?: string;
+	firstName: string;
+	id: string;
+	lastName: string;
+	middleName?: string;
 }
-
