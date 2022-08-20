40c8ade2cd4716962683a464b769f1daf5c3c051
romantsinevich <braid98@yandex.ru>
2022-08-20 16:55:27 +0500

    Add link to user page at project users table

--- src/app/modules/table/cell-components/user-info/user-info.component.ts
+++ src/app/modules/table/cell-components/user-info/user-info.component.ts
@@ -1,9 +1,11 @@
 import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
 import { ImageInfo } from '@app/models/image.model';
 import { Icons } from '@shared/modules/icons/icons';
+import { AppRoutes } from '@app/models/app-routes';
 import { TableCell } from '../../models';
 
 interface UserInfo {
+	id: string;
 	firstName: string;
 	lastName: string;
 	middleName?: string;
@@ -23,6 +25,8 @@ export interface UserInfoParams {
 	changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class UserInfoComponent implements OnInit, TableCell<UserInfo> {
+	public readonly AppRoutes = AppRoutes;
+
 	public set value(user: UserInfo) {
 		this.user = user;
 	}
