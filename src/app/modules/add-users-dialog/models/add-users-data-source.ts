32557471f31b8a9851ac0d241b4bafd02f8def58
tsinevik <braid98@yandex.ru>
2022-10-28 23:46:44 +0700

    Fix toggle in add users dialog

--- src/app/modules/add-users-dialog/models/add-users-data-source.ts
+++ src/app/modules/add-users-dialog/models/add-users-data-source.ts
@@ -1,6 +1,6 @@
 import { CollectionViewer, DataSource } from '@angular/cdk/collections';
-import { BehaviorSubject, Observable, of } from 'rxjs';
-import { map } from 'rxjs/operators';
+import { BehaviorSubject, Observable, of, Subject } from 'rxjs';
+import { map, takeUntil } from 'rxjs/operators';
 import { SelectionModel } from '@app/utils/selection-model';
 import { AddUsersApiBase } from '../services';
 import { FilterEvent } from '../../dynamic-filter/dynamic-filter.component';
@@ -12,23 +12,28 @@ export class AddUsersDataSource<T extends NewUserBase = NewUserBase> implements 
 		return this.data.asObservable();
 	}
 
+	private destroy$ = new Subject();
+
 	constructor(private apiService: AddUsersApiBase<T>, private selection: SelectionModel<T>) {}
 
 	connect(collectionViewer: CollectionViewer): Observable<T[]> {
 		return this.data.asObservable();
 	}
 
-	disconnect(collectionViewer: CollectionViewer): void {}
+	disconnect(collectionViewer: CollectionViewer): void {
+		this.destroy$.next();
+		this.destroy$.complete();
+	}
 
 	public loadUsers(filter: FilterEvent): void {
 		let users$: Observable<T[]>;
-		console.log(filter, Object.values(filter), this.isFilterEmpty(filter));
 		if (!this.isFilterEmpty(filter)) {
 			users$ = this.apiService.loadUsers(filter).pipe(map(this.mapUsersToSelected.bind(this)));
 		} else {
 			users$ = of(this.selection.selected);
 		}
-		users$.subscribe({ next: (users: T[]) => this.data.next(users) });
+		this.destroy$.next();
+		users$.pipe(takeUntil(this.destroy$)).subscribe({ next: (users: T[]) => this.data.next(users) });
 	}
 
 	public updateRow(id: string, newValue: T): void {
