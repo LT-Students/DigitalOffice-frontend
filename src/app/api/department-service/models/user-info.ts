b3d5cee37fd6f65c0720fe32ccf8a5d4e58b8f0b
tsinevik <braid98@yandex.ru>
2022-11-08 14:45:13 +0700

    Get department users avatars from separate endpoint

--- src/app/api/department-service/models/user-info.ts
+++ src/app/api/department-service/models/user-info.ts
@@ -1,17 +1,16 @@
 /* tslint:disable */
 /* eslint-disable */
 import { DepartmentUserInfo } from './department-user-info';
-import { ImageInfo } from './image-info';
 import { PositionInfo } from './position-info';
 
 /**
  * User data.
  */
 export interface UserInfo {
-	avatarImage?: ImageInfo;
 	departmentUser?: DepartmentUserInfo;
 	firstName: string;
 	id: string;
+	imageId?: string;
 	lastName: string;
 	middleName?: string;
 	position?: PositionInfo;
